0be32386e0ebd3af528028c904bfd84e12046994
ademic2022 <adeeyomichael77@gmail.com>
2023-12-12 00:57:52 +0100

    recent changes

--- src/components/home/TopCountries.jsx
+++ src/components/home/TopCountries.jsx
@@ -1,46 +1,8 @@
-// import { Avatar, Box, Stack, Typography } from "@mui/material";
-// import React from "react";
-// import { countriesData } from "../../data/TopCountries";
-
-// const TopCountries = () => {
-//   return (
-//     <Box sx={{ padding: "15px" }}>
-//       <Typography variant="h5">Top Countries</Typography>
-//       <Typography sx={{ fontSize: "12px", opacity: 0.7 }}>
-//         Sales perfomance by country
-//       </Typography>
-//       <Box sx={{ my: 2 }}>
-//         {countriesData.map(({ id, name, amount, flag }) => (
-//           <Stack
-//             direction={"row"}
-//             alignItems="center"
-//             justifyContent={"space-between"}
-//             spacing={2}
-//             key={id}
-//             sx={{ my: 3 }}
-//           >
-//             <Stack direction={"row"} alignItems="center" spacing={1}>
-//               <Avatar src={flag} sx={{ width: 30, height: 30 }} />
-//               <Typography>{name}</Typography>
-//             </Stack>
-//             <Typography sx={{ fontSize: "16px", fontWeight: "bold" }}>
-//               ${amount}
-//             </Typography>
-//           </Stack>
-//         ))}
-//       </Box>
-//     </Box>
-//   );
-// };
-
-import { Avatar, Box, Divider, Grid, Paper, Typography } from "@mui/material";
+import { Avatar, Box, Divider, Paper, Typography } from "@mui/material";
 import React from "react";
 import { FaEllipsisH } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { Users } from "../../data/customers";
-import { transactions, transactionsColumns } from "../../data/transactions";
-import Table from "../Table";
-
 const TransactionCustomer = () => {
   return (
         <Paper
@@ -103,7 +65,7 @@ const TransactionCustomer = () => {
           </Box>
           <Divider />
           <Typography variant="subtitle1" sx={{ textAlign: "center", mt: 1 }}>
-            <Link to="/customers" className="link">
+            <Link to="/teachers" className="link">
               View more
             </Link>
           </Typography>
