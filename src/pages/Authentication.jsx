179d7cebe8dabe71b715e040823b9ecc4e894a20
ademic2022 <adeeyomichael77@gmail.com>
2023-12-14 14:18:14 +0100

    sigin and registration logic

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -14,6 +14,7 @@ import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 
 
+
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
