ca36f808fa2accd256ee6df3ef69324b698acfb0
ademic2022 <adeeyomichael77@gmail.com>
2023-12-15 12:08:07 +0100

    navigation links restructuring

--- src/components/home/charts/BarChart.jsx
+++ src/components/home/charts/BarChart.jsx
@@ -1,31 +1,5 @@
 import { Paper, Typography, Grid } from "@mui/material";
-import Chart from "react-apexcharts";
-import { barChartData, barChartOptions } from "../../../data/chartData";
 
-// const BarChart = () => {
-//   return (
-//     <Paper
-//       sx={{
-//         boxShadow: "none !important",
-//         borderRadius: "12px",
-//         padding: "15px",
-//         height: { xs: "380px", md: "100%" },
-//         borderStyle: "solid",
-//         borderWidth: "1px",
-//         borderColor: "divider",
-//       }}
-//     >
-//       <Typography variant="h5">Sales Statistics</Typography>
-//       <Chart
-//         options={barChartOptions}
-//         series={barChartData}
-//         type="bar"
-//         width="100%"
-//         height="90%"
-//       />
-//     </Paper>
-//   );
-// };
 const BarChart = ({ userName, attendancePercentage }) => {
   return (
     <Paper elevation={3} style={{ padding: 20 }}>
