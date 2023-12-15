145f6c1875da1a004fe1f25039fdd3588d26c9e9
ademic2022 <adeeyomichael77@gmail.com>
2023-12-15 12:08:19 +0100

    navigation links restructuring

--- src/data/links.js
+++ src/data/links.js
@@ -11,208 +11,333 @@ import {
   FiUsers,
 } from "react-icons/fi";
 
-export const studentLinks = [
-  {
-    name: "Dashboard",
-    icon: <FiHome />,
-    url: "/",
-  },
-  {
-    name: "Activities",
-    icon: <FiShoppingBag />,
-    subLinks: [
-      {
-        name: "Teachers",
-        url: "/teachers",
-      },
-      {
-        name: "Registered Subjects",
-        url: "/subjects",
-      },
-      {
-        name: "Class Mates",
-        url: "/classmates",
-      },
-    ],
-  },
-  {
-    name: "Class Timetable",
-    icon: <FiUsers />,
-    url: "/timetable",
-  },
-  {
-    name: "Examination",
-    icon: <BsCurrencyDollar />,
-    subLinks: [
-      {
-        name: "Exam Schedules",
-        url: "/sales/analysis",
-      },
-      {
-        name: "Exam Results",
-        url: "/sales",
-      },
-    ],
-  },
-  {
-    name: "Attendance",
-    icon: <FaShare />,
-    url: "/suppliers",
-  },
-  {
-    name: "Results",
-    icon: <FaHandshake />,
-    url: "/transactions",
-  },
-  {
-    name: "Notifications",
-    icon: <FiLayers />,
-    url: "/brands",
-  },
-  {
-    name: "Reviews",
-    icon: <FiMessageCircle />,
-    subLinks: [
-      {
-        name: "Teachers Review",
-        url: "/reviews",
-      },
-      {
-        name: "Student Review",
-        url: "/products",
-      },
-    ],
-  },
-  {
-    name: "Documentation",
-    icon: <FiShoppingCart />,
-    subLinks: [
-      {
-        name: "Registration Information",
-        url: "/orders",
-      },
-      {
-        name: "Fees Breakdown",
-        url: "/orders/template",
-      },
-    ],
-  },
-  {
-    name: "Settings",
-    icon: <FiSettings />,
-    url: "/settings",
-  },
-  {
-    name: "Inbox",
-    icon: <FiMail />,
-    url: "/inbox",
-  },
-];
 
-export const teacherLinks = [
-  {
-    name: "Dashboard",
-    icon: <FiHome />,
-    url: "/",
-  },
-  {
-    name: "Activities",
-    icon: <FiShoppingBag />,
-    subLinks: [
+export const navigationLinks = (ROLE) => {
+  if (ROLE === 'Admin') {
+    return [
       {
-        name: "Students",
-        url: "/products",
+        name: "Dashboard",
+        icon: <FiHome />,
+        url: "/",
       },
       {
-        name: "Subjects Taken",
-        url: "/products/add",
+        name: "Activities",
+        icon: <FiShoppingBag />,
+        subLinks: [
+          {
+            name: "Students",
+            url: "/students",
+          },
+          {
+            name: "Teachers",
+            url: "/teachers",
+          },
+          {
+            name: "Parents",
+            url: "/parent",
+          },
+          {
+            name: "Subjects",
+            url: "/subjects",
+          },
+          {
+            name: "ClassRooms",
+            url: "/classrooms",
+          },
+          {
+            name: "Results",
+            url: "/results",
+          },
+        ],
       },
       {
-        name: "Class Rooms",
-        url: "/products/categories",
-      },
-    ],
-  },
-  {
-    name: "Class Timetable",
-    icon: <FiUsers />,
-    url: "/customers",
-  },
-  {
-    name: "Examination",
-    icon: <BsCurrencyDollar />,
-    subLinks: [
-      {
-        name: "Exam Schedules",
-        url: "/sales/analysis",
+        name: "Class Timetable",
+        icon: <FiUsers />,
+        url: "/customers",
       },
       {
-        name: "Exam Results",
-        url: "/sales",
-      },
-    ],
-  },
-  {
-    name: "Attendance",
-    icon: <FaShare />,
-    url: "/suppliers",
-    subLinks: [
-      {
-        name: "Take Attendance",
-        url: "/products",
-      },
-      {
-        name: "Attendance Record",
-        url: "/products",
-      },
-    ],
-  },
-  {
-    name: "Results",
-    icon: <FaHandshake />,
-    url: "/transactions",
-  },
-  {
-    name: "Notifications",
-    icon: <FiLayers />,
-    url: "/brands",
-  },
-  {
-    name: "Reviews",
-    icon: <FiMessageCircle />,
-    subLinks: [
-      {
-        name: "Teachers Review",
-        url: "/reviews",
+        name: "Examination",
+        icon: <BsCurrencyDollar />,
+        subLinks: [
+          {
+            name: "Exam Schedules",
+            url: "/sales/analysis",
+          },
+          {
+            name: "Exam Results",
+            url: "/sales",
+          },
+        ],
       },
       {
-        name: "Student Review",
-        url: "/reviews",
+        name: "Attendance",
+        icon: <FaShare />,
+        url: "/suppliers",
+        subLinks: [
+          {
+            name: "Take Attendance",
+            url: "/products",
+          },
+          {
+            name: "Attendance Record",
+            url: "/products",
+          },
+        ],
       },
-    ],
-  },
-  {
-    name: "Documentation",
-    icon: <FiShoppingCart />,
-    subLinks: [
       {
-        name: "Registration Information",
-        url: "/orders",
+        name: "Results",
+        icon: <FaHandshake />,
+        url: "/transactions",
       },
       {
-        name: "Fees Breakdown",
-        url: "/orders/template",
+        name: "Notifications",
+        icon: <FiLayers />,
+        url: "/brands",
       },
-    ],
-  },
-  {
-    name: "Settings",
-    icon: <FiSettings />,
-    url: "/settings",
-  },
-  {
-    name: "Inbox",
-    icon: <FiMail />,
-    url: "/inbox",
-  },
-];
+      {
+        name: "Reviews",
+        icon: <FiMessageCircle />,
+        subLinks: [
+          {
+            name: "Teachers Review",
+            url: "/reviews",
+          },
+          {
+            name: "Student Review",
+            url: "/reviews",
+          },
+        ],
+      },
+      {
+        name: "Documentation",
+        icon: <FiShoppingCart />,
+        subLinks: [
+          {
+            name: "Registration Information",
+            url: "/orders",
+          },
+          {
+            name: "Fees Breakdown",
+            url: "/orders/template",
+          },
+        ],
+      },
+      {
+        name: "Settings",
+        icon: <FiSettings />,
+        url: "/settings",
+      },
+      {
+        name: "Inbox",
+        icon: <FiMail />,
+        url: "/inbox",
+      },
+    ];
+  } else if (ROLE === 'Teacher') {
+    return [
+      {
+        name: "Dashboard",
+        icon: <FiHome />,
+        url: "/",
+      },
+      {
+        name: "Activities",
+        icon: <FiShoppingBag />,
+        subLinks: [
+          {
+            name: "Students",
+            url: "/products",
+          },
+          {
+            name: "Subjects Taken",
+            url: "/products/add",
+          },
+          {
+            name: "Class Rooms",
+            url: "/products/categories",
+          },
+        ],
+      },
+      {
+        name: "Class Timetable",
+        icon: <FiUsers />,
+        url: "/customers",
+      },
+      {
+        name: "Examination",
+        icon: <BsCurrencyDollar />,
+        subLinks: [
+          {
+            name: "Exam Schedules",
+            url: "/sales/analysis",
+          },
+          {
+            name: "Exam Results",
+            url: "/sales",
+          },
+        ],
+      },
+      {
+        name: "Attendance",
+        icon: <FaShare />,
+        url: "/suppliers",
+        subLinks: [
+          {
+            name: "Take Attendance",
+            url: "/products",
+          },
+          {
+            name: "Attendance Record",
+            url: "/products",
+          },
+        ],
+      },
+      {
+        name: "Results",
+        icon: <FaHandshake />,
+        url: "/transactions",
+      },
+      {
+        name: "Notifications",
+        icon: <FiLayers />,
+        url: "/brands",
+      },
+      {
+        name: "Reviews",
+        icon: <FiMessageCircle />,
+        subLinks: [
+          {
+            name: "Teachers Review",
+            url: "/reviews",
+          },
+          {
+            name: "Student Review",
+            url: "/reviews",
+          },
+        ],
+      },
+      {
+        name: "Documentation",
+        icon: <FiShoppingCart />,
+        subLinks: [
+          {
+            name: "Registration Information",
+            url: "/orders",
+          },
+          {
+            name: "Fees Breakdown",
+            url: "/orders/template",
+          },
+        ],
+      },
+      {
+        name: "Settings",
+        icon: <FiSettings />,
+        url: "/settings",
+      },
+      {
+        name: "Inbox",
+        icon: <FiMail />,
+        url: "/inbox",
+      },
+    ];
+  } else {
+    return [
+      {
+        name: "Dashboard",
+        icon: <FiHome />,
+        url: "/",
+      },
+      {
+        name: "Activities",
+        icon: <FiShoppingBag />,
+        subLinks: [
+          {
+            name: "Teachers",
+            url: "/teachers",
+          },
+          {
+            name: "Registered Subjects",
+            url: "/subjects",
+          },
+          {
+            name: "Class Mates",
+            url: "/classmates",
+          },
+        ],
+      },
+      {
+        name: "Class Timetable",
+        icon: <FiUsers />,
+        url: "/timetable",
+      },
+      {
+        name: "Examination",
+        icon: <BsCurrencyDollar />,
+        subLinks: [
+          {
+            name: "Exam Schedules",
+            url: "/sales/analysis",
+          },
+          {
+            name: "Exam Results",
+            url: "/sales",
+          },
+        ],
+      },
+      {
+        name: "Attendance",
+        icon: <FaShare />,
+        url: "/suppliers",
+      },
+      {
+        name: "Results",
+        icon: <FaHandshake />,
+        url: "/transactions",
+      },
+      {
+        name: "Notifications",
+        icon: <FiLayers />,
+        url: "/brands",
+      },
+      {
+        name: "Reviews",
+        icon: <FiMessageCircle />,
+        subLinks: [
+          {
+            name: "Teachers Review",
+            url: "/reviews",
+          },
+          {
+            name: "Student Review",
+            url: "/products",
+          },
+        ],
+      },
+      {
+        name: "Documentation",
+        icon: <FiShoppingCart />,
+        subLinks: [
+          {
+            name: "Registration Information",
+            url: "/orders",
+          },
+          {
+            name: "Fees Breakdown",
+            url: "/orders/template",
+          },
+        ],
+      },
+      {
+        name: "Settings",
+        icon: <FiSettings />,
+        url: "/settings",
+      },
+      {
+        name: "Inbox",
+        icon: <FiMail />,
+        url: "/inbox",
+      },
+    ];
+  }
+};
