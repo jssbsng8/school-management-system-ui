6825eb394d282a3b0403019562ab83834f049430
ademic2022 <adeeyomichael77@gmail.com>
2023-12-17 17:03:23 +0100

    remodified user login data using react context

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import "./styles/index.css";
 import App from "./App";
 import { ThemeToggleProvider } from "./contexts/ThemeContext";
 import { CssBaseline } from "@mui/material";
+import { UserProvider } from "./components/utils/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeToggleProvider>
       <CssBaseline />
+        <UserProvider>
       <Router>
-        <App />
+          <App />
       </Router>
+        </UserProvider>
     </ThemeToggleProvider>
   </React.StrictMode>
 );
