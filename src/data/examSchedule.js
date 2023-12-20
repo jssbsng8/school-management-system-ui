e271a4a0beafbcda1ef9c6f53ed2672a23f4ae1b
ademic2022 <adeeyomichael77@gmail.com>
2023-12-20 01:34:48 +0100

    interface restructuring

---
+++ src/data/examSchedule.js
@@ -0,0 +1,27 @@
+export const examScheduleColumn = [
+    {
+        accessorKey: "subject",
+        header: "Subjects",
+    },
+    {
+        accessorKey: "date",
+        header: "Exam Date",
+    },
+    {
+        accessorKey: "time",
+        header: "Exam Time",
+    },
+  ];
+
+export const examScheduleData = [
+    { id: 1, subject: "Mathematics", date: "2023-01-15", time: "09:00 AM" },
+    { id: 2, subject: "Science", date: "2023-01-17", time: "10:30 AM" },
+    { id: 3, subject: "History", date: "2023-01-20", time: "02:00 PM" },
+    { id: 4, subject: "English", date: "2023-01-22", time: "11:00 AM" },
+    { id: 5, subject: "Physics", date: "2023-01-25", time: "03:30 PM" },
+    { id: 6, subject: "Chemistry", date: "2023-01-28", time: "01:45 PM" },
+    { id: 7, subject: "Biology", date: "2023-01-30", time: "09:30 AM" },
+    { id: 8, subject: "Computer Science", date: "2023-02-02", time: "03:00 PM" },
+    { id: 9, subject: "Geography", date: "2023-02-05", time: "10:00 AM" },
+    { id: 10, subject: "Economics", date: "2023-02-08", time: "02:30 PM" },
+];
