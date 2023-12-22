3d04055f4cb7af149855f71705c3c9dc8169dce5
ademic2022 <adeeyomichael77@gmail.com>
2023-12-22 15:59:16 +0100

    reset password implemented

--- src/components/utils/userContext.js
+++ src/components/utils/userContext.js
@@ -1,5 +1,97 @@
+// import React, { createContext, useContext, useState, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { athenticatedUser, get_enrolled_subjects, get_assigned_subjects } from '../../apiCalls/authApi';
+// import { USER_ENDPOINTS, CORE } from '../../apiCalls/endpoints';
+
+// const UserContext = createContext();
+
+// export const UserProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+//   const [auth, setAuth] = useState(false);
+//   const [role, setRole] = useState(null);
+//   const [subjects, setSubject] = useState(null);
+//   const navigate = useNavigate();
+  
+
+//   useEffect(() => {
+//     const fetchAuthenticatedUser = async () => {
+//       try {
+//         const getUser = await athenticatedUser(USER_ENDPOINTS.AUTHENTICATED_USER);        
+//         if (getUser) {
+//           const parsedUserData = JSON.parse(getUser);
+//           setUser(parsedUserData);
+//           setAuth(true);
+//           setRole(parsedUserData.role);
+//           localStorage.setItem('userData', JSON.stringify({
+//             'auth': true,
+//             'role': parsedUserData.role
+//           }));
+          
+//         } else {
+//           setUserContext(null, false, null);
+//           localStorage.clear()
+//           navigate('/login');
+//         }
+//       } catch (error) {
+//         console.error("Error fetching authenticated user:", error);
+//       }
+//     };
+//     fetchAuthenticatedUser();
+//     // console.log(role);
+//   }, [navigate]);
+
+
+//   useEffect(()=> {
+//     const fetchData = async () => {
+//       try {
+//         const userData = JSON.parse(localStorage.getItem('userData'));
+//         const getRole = userData ? userData.role : null;
+  
+//         if (getRole === "Student") {
+//           const enrolledSubjectsJsonString = await get_enrolled_subjects(CORE.GET_ENROLLED_SUBJECTS);
+//           const enrolledSubjects = JSON.parse(enrolledSubjectsJsonString);
+//           setSubject(enrolledSubjects);
+//         }
+//         else if (getRole === "Teacher"){
+//           const assignedSubjectsJsonString = await get_assigned_subjects(CORE.GET_ASSIGNED_SUBJECTS);
+//           const assignedSubjects = JSON.parse(assignedSubjectsJsonString);
+//           setSubject(assignedSubjects);
+//         }
+//       } catch (error) {
+//         console.error('Error fetching enrolled subjects:', error);
+//       }
+//     };
+  
+//     fetchData();
+//   },[])
+  
+//   const setUserContext = (userData, authStatus, UserRole) => {
+//     setUser(userData);
+//     setAuth(authStatus);
+//     setRole(UserRole);
+//   };
+
+//   const setAuthStatus = (newAuthStatus) => {
+//     setAuth(newAuthStatus);
+//   };
+  
+//   return (
+//     <UserContext.Provider value={{ user, auth, role, subjects, setUserContext, setAuthStatus, setRole }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+
+// export const useUser = () => {
+//   const context = useContext(UserContext);
+//   if (!context) {
+//     throw new Error('useUser must be used within a UserProvider');
+//   }
+//   return context;
+// };
+
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { athenticatedUser, get_enrolled_subjects, get_assigned_subjects } from '../../apiCalls/authApi';
 import { USER_ENDPOINTS, CORE } from '../../apiCalls/endpoints';
 
@@ -11,6 +103,7 @@ export const UserProvider = ({ children }) => {
   const [role, setRole] = useState(null);
   const [subjects, setSubject] = useState(null);
   const navigate = useNavigate();
+  const location = useLocation();
   
 
   useEffect(() => {
@@ -29,16 +122,21 @@ export const UserProvider = ({ children }) => {
           
         } else {
           setUserContext(null, false, null);
-          localStorage.clear()
-          navigate('/login');
+          localStorage.clear() 
+          // navigate("/login") 
+          const currentRoute = location.pathname;
+          const excludedRoutes = ['/login', '/success']; // Add routes to exclude from navigation
+          if (!excludedRoutes.includes(currentRoute)) {
+            navigate("/login");
+          } 
         }
       } catch (error) {
         console.error("Error fetching authenticated user:", error);
       }
     };
+    
     fetchAuthenticatedUser();
-    // console.log(role);
-  }, [navigate]);
+  }, [navigate, location.pathname]);
 
 
   useEffect(()=> {
@@ -64,7 +162,7 @@ export const UserProvider = ({ children }) => {
   
     fetchData();
   },[])
-  
+
   const setUserContext = (userData, authStatus, UserRole) => {
     setUser(userData);
     setAuth(authStatus);
