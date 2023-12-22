f59cfd1215a4809b5797ec97d9694fed6133e5d3
ademic2022 <adeeyomichael77@gmail.com>
2023-12-22 01:50:37 +0100

    more modifications

--- src/apiCalls/authApi.js
+++ src/apiCalls/authApi.js
@@ -1,5 +1,6 @@
 import { errorToast } from "../components/utils/toastUtils";
 
+
 // ======================= GET AUTHENTICATED USER DATA =======================
 
 export const athenticatedUser = async (url) => {
@@ -17,7 +18,7 @@ export const athenticatedUser = async (url) => {
             return JSON.stringify(responseData);
         } 
         else {
-            return
+            return null
         }
     } catch (error) {
         const message = `Error during logout: ${error}`
@@ -25,4 +26,55 @@ export const athenticatedUser = async (url) => {
     }
 }
 
+export const get_assigned_subjects = async(url) => {
+    console.log(`Token ${localStorage.getItem('token')}`);
+    try{
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Token ${localStorage.getItem('token')}`,
+            }
+        });
+        if(response.ok){
+            const responseData = await (response.json())
+            return JSON.stringify(responseData);
+        }
+        else{
+            const errorMessage = await response.text();
+            console.log(errorMessage);
+            errorToast(errorMessage)
+        }
+    }
+    catch (error){
+        const message = `Error getting assigned subjects: ${error}`
+        errorToast(message)
+    }
+}
 
+
+export const get_enrolled_subjects = async(url) => {
+    console.log(`Token ${localStorage.getItem('token')}`);
+    try{
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Token ${localStorage.getItem('token')}`,
+            }
+        });
+        if(response.ok){
+            const responseData = await (response.json())
+            return JSON.stringify(responseData);
+        }
+        else{
+            const errorMessage = await response.text();
+            console.log(errorMessage);
+            return errorMessage
+        }
+    }
+    catch (error){
+        const message = `Error getting enrolled subjects: ${error}`
+        errorToast(message)
+    }
+}
