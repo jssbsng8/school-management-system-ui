2b5be03bf667813f302cec9c63b9e485cb8d5af1
ademic2022 <adeeyomichael77@gmail.com>
2023-12-23 13:25:43 +0100

    modifications

--- src/components/home/charts/BarChart.jsx
+++ src/components/home/charts/BarChart.jsx
@@ -1,7 +1,8 @@
-import { Paper, Typography, Grid } from "@mui/material";
+import { Paper, Typography, Grid, colors } from "@mui/material";
+import { useUser } from "../../utils/userContext";
 
-const BarChart = ({ userName, attendancePercentage }) => {
-  const role = localStorage.getItem
+const BarChart = ({ fullName, attendancePercentage, userName }) => {
+  const { role } = useUser()
   return (
     <Paper elevation={3} style={{ padding: 20 }}>
       <Grid container spacing={2} alignItems="center">
@@ -16,17 +17,30 @@ const BarChart = ({ userName, attendancePercentage }) => {
         </Grid>
 
         {/* Right column for the welcome message */}
+        {role === "Student" && (
         <Grid item xs={12} sm={8}>
           <Typography variant="h4" gutterBottom>
-            Welcome, {userName}! Keep Going.
+            Welcome, {fullName}! Keep Going.
           </Typography>
-          {
           <Typography paragraph>
             Your current attendance is {attendancePercentage.toFixed(2)}%, which is{' '}
             {attendancePercentage > 75 ? 'above' : 'below or equal to'} 75.00% of the minimum attendance mark.
           </Typography>
-          }
         </Grid>
+      )}
+
+      {role === "Teacher" && (
+        <Grid item xs={12} sm={8}>
+          <Typography variant="h4" gutterBottom>
+          Welcome, Mr. {userName.toUpperCase()}! Ready to Inspire.
+          </Typography>
+          <Typography paragraph>
+            You are currently assigned to teach <span style={{ color: 'green' }}>Mathematics and Biology.</span> Keep track of your class schedules,
+            manage student performance, and mark attendance to ensure a productive learning environment.
+            Your dedication makes a difference!
+          </Typography>
+        </Grid>
+      )}
       </Grid>
     </Paper>
   );
