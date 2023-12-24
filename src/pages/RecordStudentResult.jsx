7fa038708709d53c65b1bfbd6f67a665f8a1478f
ademic2022 <adeeyomichael77@gmail.com>
2023-12-24 11:11:27 +0100

    multi records for student grades

--- src/pages/RecordStudentResult.jsx
+++ src/pages/RecordStudentResult.jsx
@@ -1,214 +1,4 @@
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
-// import AddIcon from '@mui/icons-material/Add';
-// import EditIcon from '@mui/icons-material/Edit';
-// import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-// import SaveIcon from '@mui/icons-material/Save';
-// import CancelIcon from '@mui/icons-material/Close';
-// import {
-//   GridRowModes,
-//   DataGrid,
-//   GridToolbarContainer,
-//   GridActionsCellItem,
-//   GridRowEditStopReasons,
-// } from '@mui/x-data-grid';
-
-
-// const myData = [
-//   {
-//     id: 1,
-//     name: 'John Doe',
-//     age: 30,
-//     joinDate: '2022-01-01',
-//     role: 'Development',
-//   },
-//   {
-//     id: 2,
-//     name: 'Jane Smith',
-//     age: 25,
-//     joinDate: '2022-02-15',
-//     role: 'Finance',
-//   },
-// ];
-
-// function EditToolbar(props) {
-//   const { setRows, setRowModesModel } = props;
-
-//   const handleClick = () => {
-//     const id = myData.length + 1; // Adjust the ID generation logic based on your actual data source
-//     setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
-//     setRowModesModel((oldModel) => ({
-//       ...oldModel,
-//       [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-//     }));
-//   };
-
-//   return (
-//     <GridToolbarContainer>
-//       <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-//         Add record
-//       </Button>
-//     </GridToolbarContainer>
-//   );
-// }
-
-// export default function RecordStudentResult() {
-//   const [rows, setRows] = React.useState(myData);
-//   const [rowModesModel, setRowModesModel] = React.useState({});
-
-//   const handleRowEditStop = (params, event) => {
-//     if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-//       event.defaultMuiPrevented = true;
-//     }
-//   };
-
-//   const handleEditClick = (id) => () => {
-//     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-//   };
-
-//   const handleSaveClick = (id) => () => {
-//     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-//   };
-
-//   const handleDeleteClick = (id) => () => {
-//     setRows(rows.filter((row) => row.id !== id));
-//   };
-
-//   const handleCancelClick = (id) => () => {
-//     setRowModesModel({
-//       ...rowModesModel,
-//       [id]: { mode: GridRowModes.View, ignoreModifications: true },
-//     });
-
-//     const editedRow = rows.find((row) => row.id === id);
-
-//     if (editedRow.isNew) {
-//       setRows(rows.filter((row) => row.id !== id));
-//     }
-//   };
-
-//   const processRowUpdate = (newRow) => {
-//     const updatedRow = { ...newRow, isNew: false };
-//     setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-//     return updatedRow;
-//   };
-
-//   const handleRowModesModelChange = (newRowModesModel) => {
-//     setRowModesModel(newRowModesModel);
-//   };
-
-//   const columns = [
-//     { field: 'name', headerName: 'Name', width: 180, editable: true },
-//     {
-//       field: 'age',
-//       headerName: 'Age',
-//       type: 'number',
-//       width: 80,
-//       align: 'left',
-//       headerAlign: 'left',
-//       editable: true,
-//     },
-//     {
-//       field: 'joinDate',
-//       headerName: 'Join date',
-//       type: 'date',
-//       width: 180,
-//       editable: true,
-//       valueGetter: (params) => new Date(params.value),
-//     },
-//     {
-//       field: 'role',
-//       headerName: 'Department',
-//       width: 220,
-//       editable: true,
-//       type: 'singleSelect',
-//       valueOptions: ['Market', 'Finance', 'Development'],
-//     },
-//     {
-//       field: 'actions',
-//       type: 'actions',
-//       headerName: 'Actions',
-//       width: 100,
-//       cellClassName: 'actions',
-//       getActions: ({ id }) => {
-//         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-//         if (isInEditMode) {
-//           return [
-//             <GridActionsCellItem
-//               icon={<SaveIcon />}
-//               label="Save"
-//               sx={{
-//                 color: 'primary.main',
-//               }}
-//               onClick={handleSaveClick(id)}
-//             />,
-//             <GridActionsCellItem
-//               icon={<CancelIcon />}
-//               label="Cancel"
-//               className="textPrimary"
-//               onClick={handleCancelClick(id)}
-//               color="inherit"
-//             />,
-//           ];
-//         }
-
-//         return [
-//           <GridActionsCellItem
-//             icon={<EditIcon />}
-//             label="Edit"
-//             className="textPrimary"
-//             onClick={handleEditClick(id)}
-//             color="inherit"
-//           />,
-//           <GridActionsCellItem
-//             icon={<DeleteIcon />}
-//             label="Delete"
-//             onClick={handleDeleteClick(id)}
-//             color="inherit"
-//           />,
-//         ];
-//       },
-//     },
-//   ];
-
-//   return (
-//     <Box
-//       sx={{
-//         pt: "80px",
-//         height: 500,
-//         width: '100%',
-//         '& .actions': {
-//           color: 'text.secondary',
-//         },
-//         '& .textPrimary': {
-//           color: 'text.primary',
-//         },
-//       }}
-//     >
-//       <DataGrid
-//         rows={rows}
-//         columns={columns}
-//         editMode="row"
-//         rowModesModel={rowModesModel}
-//         onRowModesModelChange={handleRowModesModelChange}
-//         onRowEditStop={handleRowEditStop}
-//         processRowUpdate={processRowUpdate}
-//         slots={{
-//           toolbar: EditToolbar,
-//         }}
-//         slotProps={{
-//           toolbar: { setRows, setRowModesModel },
-//         }}
-//       />
-//     </Box>
-//   );
-// }
-
-
-// RecordStudentResult.js
-import React from 'react';
+import React, { useState} from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
@@ -216,8 +6,10 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { Typography } from "@mui/material";
 import { ToastContainer } from 'react-toastify';
+import { successToast } from '../components/utils/toastUtils';
 import {
   GridRowModes,
   DataGrid,
@@ -225,14 +17,25 @@ import {
   GridActionsCellItem,
   GridRowEditStopReasons,
 } from '@mui/x-data-grid';
+import { randomId } from '@mui/x-data-grid-generator';
 import { myData, myColumns } from '../data/recordResult';
 
 function EditToolbar(props) {
   const { setRows, setRowModesModel } = props;
 
   const handleClick = () => {
-    const id = myData.length + 1;
-    setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
+    const id = randomId();
+    setRows((oldRows) => [
+        ...oldRows,
+        {
+          id,
+          name: '',
+          class: '',
+          exam_type: '',
+          subject: '',
+          score: '',
+        },
+      ]);
     setRowModesModel((oldModel) => ({
       ...oldModel,
       [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
@@ -248,7 +51,8 @@ function EditToolbar(props) {
   );
 }
 
-export default function RecordStudentResult() {
+const RecordStudentResult = () => {
+  const [loading, setLoading] = useState(false);
   const [rows, setRows] = React.useState(myData);
   const [rowModesModel, setRowModesModel] = React.useState({});
 
@@ -291,6 +95,16 @@ export default function RecordStudentResult() {
 
   const handleRowModesModelChange = (newRowModesModel) => {
     setRowModesModel(newRowModesModel);
+  };
+
+  const handleGetAllData = async () => {
+    setLoading(true)
+    // Access the current rows data
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    console.log(rows);
+    successToast("Results Submitted")
+    setRows(myData)
+    setLoading(false)
   };
 
   const columns = [
@@ -350,7 +164,6 @@ export default function RecordStudentResult() {
             flexDirection: "column",
             pt: "80px",
             pb: "20px",
-            // height: 500,
             width: '100%',
             '& .actions': {
             color: 'text.secondary',
@@ -379,7 +192,18 @@ export default function RecordStudentResult() {
             toolbar: { setRows, setRowModesModel },
             }}
         />
+        <LoadingButton 
+            variant="contained" 
+            type="submit"
+            onClick={handleGetAllData} 
+            loading={loading}
+            loadingPosition="start"
+            sx={{ mt: 3, mb: 2, width: "15%" }}
+            >
+            {loading ? 'Submitting...' : 'Submit Record'}
+        </LoadingButton>
     </Box>
   );
 }
 
+export default RecordStudentResult
