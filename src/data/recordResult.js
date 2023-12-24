9b0b86f83b24e05642b1d94660ee5e4bba6c4991
ademic2022 <adeeyomichael77@gmail.com>
2023-12-24 02:07:49 +0100

    add result record for student

---
+++ src/data/recordResult.js
@@ -0,0 +1,67 @@
+// MyColumns.js
+export const myColumns = [
+  { 
+    field: 'name', 
+    headerName: 'Student Name', 
+    width: 180,
+    type: 'singleSelect',
+    editable: true,
+    valueOptions: ['John Doe', 'Jane Smith', 'Anthony Williams'],
+  },
+  {
+    field: 'class',
+    headerName: 'Class',
+    type: 'singleSelect',
+    width: 320,
+    align: 'left',
+    valueOptions: ['JUNIOR SECONDARY SCHOOL 3', 'JUNIOR SECONDARY SCHOOL 1'],
+    editable: true,
+  },
+  {
+    field: 'exam_type',
+    headerName: 'Exam Type',
+    width: 180,
+    editable: true,
+    type: 'singleSelect',
+    valueOptions: ['CA-Test', 'Examination'],
+  },
+  {
+    field: 'subject',
+    headerName: 'Subject',
+    type: 'singleSelect',
+    width: 180,
+    editable: true,
+    valueOptions: ['Biology', 'Mathematics'],
+  },
+  {
+    field: 'score',
+    headerName: 'Score',
+    width: 150,
+    editable: true,
+    type: 'number',
+    align: 'left',
+    headerAlign: 'left',
+  },
+];
+
+
+
+// MyData.js
+export const myData = [
+  {
+    id: 1,
+    name: 'John Doe',
+    class: "JUNIOR SECONDARY SCHOOL 1",
+    exam_type: 'CA-Test',
+    subject: "Biology",
+    score: 50,
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    class: "JUNIOR SECONDARY SCHOOL 1",
+    exam_type: 'CA-Test',
+    subject: "Biology",
+    score: 67,
+  },
+];
