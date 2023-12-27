b296d944a215a088d297d90fb137182b36da43dc
ademic2022 <adeeyomichael77@gmail.com>
2023-12-27 13:57:22 +0100

    code refactoring

--- src/pages/CheckResult.jsx
+++ src/pages/CheckResult.jsx
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import {Table} from "../components/Table";
 import { resultsData, resultColumn } from '../data/results';
-import RecordResultForm from "../components/CheckResultForm";
+import RecordResultForm from "../components/forms/CheckResultForm";
 import { errorToast, successToast } from "../components/utils/toastUtils";
 
 
