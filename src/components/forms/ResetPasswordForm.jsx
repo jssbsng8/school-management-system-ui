0fe413ce134983a007f401cf086c8b0a0e6adf10
ademic2022 <adeeyomichael77@gmail.com>
2023-12-27 13:57:09 +0100

    code refactoring

---
+++ src/components/forms/ResetPasswordForm.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import { successToast, errorToast } from '../utils/toastUtils';
+import { isEmailValid } from '../utils/dataValidator';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useNavigate } from "react-router-dom";
+
+
+const ResetPasswordForm = ({ onSubmit, onCancel }) => {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleResetPassword = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        // Add logic for handling login data
+        const email = event.target.elements.registerEmail.value;
+        const validated = isEmailValid(email)
+        if (validated[1] === 'error'){
+            errorToast(validated[0])
+            setLoading(false);
+        }
+        else{
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            navigate("/reset_password")
+            successToast("Reset link has been sent to your email address!")
+            setLoading(false);
+        }
+    }
+    return (
+        <Box component="form" noValidate onSubmit={handleResetPassword} sx={{ mt: 5 }}>
+        {/* Reset Password form fields */}
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="registerEmail"
+            label="Email Address"
+            name="registerEmail"
+            autoComplete="email"
+        />
+        <LoadingButton
+            type="submit"
+            fullWidth
+            loading={loading}
+            loadingPosition="start"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+        >
+            {loading ? 'Please Wait...' : 'Send Reset Link'}
+        </LoadingButton>
+        <Link component="button" variant="body2" onClick={onCancel}>
+            Back to Login
+        </Link>
+        </Box>
+    );
+};
+
+export default ResetPasswordForm;
