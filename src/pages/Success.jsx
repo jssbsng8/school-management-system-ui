e4bcc0e67a8dcecfc4c647515e68361ad3999b9d
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 13:02:14 +0100

    code reformatting

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const SuccessPage = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
       }}
     >
-      <Paper elevation={6} sx={{ padding: 3, textAlign: 'center', boxShadow: 3, backgroundColor: '#fff' }}>
+      <Paper
+        elevation={6}
+        sx={{
+          padding: 3,
+          textAlign: "center",
+          boxShadow: 3,
+          backgroundColor: "#fff",
+        }}
+      >
         {/* Add boxShadow for a subtle shadow effect */}
         <Typography variant="h4" color="green" gutterBottom>
-            Registration Successful
+          Registration Successful
         </Typography>
         <Typography variant="body1" paragraph>
-            We appreciate your registration.
+          We appreciate your registration.
         </Typography>
         <Typography variant="body2" color="green" paragraph>
-            An email with further instructions has been sent to your email address.
+          An email with further instructions has been sent to your email
+          address.
         </Typography>
         <Button
-            component={Link}
-            to="/login"
-            variant="contained"
-            color="primary"
+          component={Link}
+          to="/login"
+          variant="contained"
+          color="primary"
         >
-            Go to Login
+          Go to Login
         </Button>
       </Paper>
     </Box>
