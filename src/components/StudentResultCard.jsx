de661d567a1144e7137b4fc729d71af5969da03f
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:56:09 +0100

    code reformatting

--- src/components/StudentResultCard.jsx
+++ src/components/StudentResultCard.jsx
@@ -3,37 +3,30 @@ import { Paper, Typography, Grid } from "@mui/material";
 const StudentResultCard = ({ userName, attendancePercentage }) => {
   return (
     <Paper elevation={3} style={{ padding: 20 }}>
-  <Grid container spacing={2} alignItems="center">
-
-    <Grid item xs={12} sm={8}>
-        {/* Student information */}
-        <Typography variant="h6" gutterBottom>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={12} sm={8}>
+          {/* Student information */}
+          <Typography variant="h6" gutterBottom>
             Student Information
-        </Typography>
-        <Typography>
-            Registration Number: AGE/2019/002
-        </Typography>
-        <Typography>
-            Name: {userName.toUpperCase()}
-        </Typography>
-        <Typography>
+          </Typography>
+          <Typography>Registration Number: AGE/2019/002</Typography>
+          <Typography>Name: {userName.toUpperCase()}</Typography>
+          <Typography>
             Session/Semester: HARMATTAN SEMESTER 2022/2023 SESSION
-        </Typography>
-        <Typography>
-            Current Class: Junior Secondary School 3
-        </Typography>
+          </Typography>
+          <Typography>Current Class: Junior Secondary School 3</Typography>
 
-        <Typography variant="h6" gutterBottom>
+          <Typography variant="h6" gutterBottom>
             Result Summary
-        </Typography>
-        <Typography paragraph>
-            Your current attendance is {attendancePercentage.toFixed(2)}%, which is{' '}
-            {attendancePercentage > 75 ? 'above' : 'below or equal to'} 75.00% of the minimum attendance mark.
-        </Typography>
-    </Grid>
-  </Grid>
-</Paper>
-
+          </Typography>
+          <Typography paragraph>
+            Your current attendance is {attendancePercentage.toFixed(2)}%, which
+            is {attendancePercentage > 75 ? "above" : "below or equal to"}{" "}
+            75.00% of the minimum attendance mark.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 export default StudentResultCard;
