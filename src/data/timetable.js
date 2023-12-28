e4bdfea38dcf0e0b5d10905af6bf62c720ac30e5
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:57:19 +0100

    code reformatting

--- src/data/timetable.js
+++ src/data/timetable.js
@@ -1,32 +1,47 @@
 export const timetableColumns = [
-    {
-      accessorKey: "time",
-      header: "Time",
-    },
-    {
-      accessorKey: "monday",
-      header: "Monday",
-    },
-    {
-      accessorKey: "tuesday",
-      header: "Tuesday",
-    },
-    {
-      accessorKey: "wednesday",
-      header: "Wednesday",
-    },
-    {
-      accessorKey: "thursday",
-      header: "Thursday",
-    },
-    {
-      accessorKey: "friday",
-      header: "Friday",
-    },
-  ];
-
+  {
+    accessorKey: "time",
+    header: "Time",
+  },
+  {
+    accessorKey: "monday",
+    header: "Monday",
+  },
+  {
+    accessorKey: "tuesday",
+    header: "Tuesday",
+  },
+  {
+    accessorKey: "wednesday",
+    header: "Wednesday",
+  },
+  {
+    accessorKey: "thursday",
+    header: "Thursday",
+  },
+  {
+    accessorKey: "friday",
+    header: "Friday",
+  },
+];
 
 export const timetableData = [
-    { id: 1, time: "8:00 AM - 9:00 AM", monday: "Math", tuesday: "English", wednesday: "Science", thursday: "History", friday: "Gym" },
-    { id: 2, time: "9:00 AM - 10:00 AM", monday: "History", tuesday: "Math", wednesday: "English", thursday: "Science", friday: "Gym" },
+  {
+    id: 1,
+    time: "8:00 AM - 9:00 AM",
+    monday: "Math",
+    tuesday: "English",
+    wednesday: "Science",
+    thursday: "History",
+    friday: "Gym",
+  },
+  {
+    id: 2,
+    time: "9:00 AM - 10:00 AM",
+    monday: "History",
+    tuesday: "Math",
+    wednesday: "English",
+    thursday: "Science",
+    friday: "Gym",
+  },
 ];
