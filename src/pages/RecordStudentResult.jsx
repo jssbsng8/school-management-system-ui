f732fe146e5f36159e8ec3ef5d16f950887b295a
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:47:52 +0100

    code refactoring and tableEditable

--- src/pages/RecordStudentResult.jsx
+++ src/pages/RecordStudentResult.jsx
@@ -1,209 +1,34 @@
-import React, { useState} from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Close';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { Typography } from "@mui/material";
-import { ToastContainer } from 'react-toastify';
-import { successToast } from '../components/utils/toastUtils';
-import {
-  GridRowModes,
-  DataGrid,
-  GridToolbarContainer,
-  GridActionsCellItem,
-  GridRowEditStopReasons,
-} from '@mui/x-data-grid';
-import { randomId } from '@mui/x-data-grid-generator';
-import { myData, myColumns } from '../data/recordResult';
-
-function EditToolbar(props) {
-  const { setRows, setRowModesModel } = props;
-
-  const handleClick = () => {
-    const id = randomId();
-    setRows((oldRows) => [
-        ...oldRows,
-        {
-          id,
-          name: '',
-          class: '',
-          exam_type: '',
-          subject: '',
-          score: '',
-        },
-      ]);
-    setRowModesModel((oldModel) => ({
-      ...oldModel,
-      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-    }));
-  };
-
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import TableEditable from "../components/TableEditable";
+import { myData, myColumns } from "../data/recordResult";
+const LeaveRequests = () => {
   return (
-    <GridToolbarContainer>
-      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-        Add record
-      </Button>
-    </GridToolbarContainer>
-  );
-}
-
-const RecordStudentResult = () => {
-  const [loading, setLoading] = useState(false);
-  const [rows, setRows] = React.useState(myData);
-  const [rowModesModel, setRowModesModel] = React.useState({});
-
-  const handleRowEditStop = (params, event) => {
-    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-      event.defaultMuiPrevented = true;
-    }
-  };
-
-  const handleEditClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-  };
-
-  const handleSaveClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
-
-  const handleDeleteClick = (id) => () => {
-    setRows(rows.filter((row) => row.id !== id));
-  };
-
-  const handleCancelClick = (id) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-
-    const editedRow = rows.find((row) => row.id === id);
-
-    if (editedRow.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
-    }
-  };
-
-  const processRowUpdate = (newRow) => {
-    const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-    return updatedRow;
-  };
-
-  const handleRowModesModelChange = (newRowModesModel) => {
-    setRowModesModel(newRowModesModel);
-  };
-
-  const handleGetAllData = async () => {
-    setLoading(true)
-    // Access the current rows data
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    console.log(rows);
-    successToast("Results Submitted")
-    setRows(myData)
-    setLoading(false)
-  };
-
-  const columns = [
-    ...myColumns,
-    {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      width: 100,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label="Save"
-              sx={{
-                color: 'primary.main',
-              }}
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
-        }
-
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    },
-  ];
-
-  return (
-    <Box
+    <Box sx={{ pt: "80px", pb: "20px" }}>
+      <Box
         sx={{
-            display: "flex", gap: 2,
-            flexDirection: "column",
-            pt: "80px",
-            pb: "20px",
-            width: '100%',
-            '& .actions': {
-            color: 'text.secondary',
-            },
-            '& .textPrimary': {
-            color: 'text.primary',
-            },
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: "16px",
         }}
-    >
-        <ToastContainer />
-        <Typography variant="h6" sx={{ marginBottom: "14px" }}>
-            Record Student Results
-        </Typography>
-        <DataGrid
-            rows={rows}
-            columns={columns}
-            editMode="row"
-            rowModesModel={rowModesModel}
-            onRowModesModelChange={handleRowModesModelChange}
-            onRowEditStop={handleRowEditStop}
-            processRowUpdate={processRowUpdate}
-            slots={{
-            toolbar: EditToolbar,
-            }}
-            slotProps={{
-            toolbar: { setRows, setRowModesModel },
-            }}
+      >
+        <Typography variant="h6">Record Student Results</Typography>
+      </Box>
+      {myData ? (
+        <TableEditable
+          myData={myData}
+          myColumns={myColumns}
+          enableSubmitButton={true}
+          enableAddNewRow={true}
         />
-        <LoadingButton 
-            variant="contained" 
-            type="submit"
-            onClick={handleGetAllData} 
-            loading={loading}
-            loadingPosition="start"
-            sx={{ mt: 3, mb: 2, width: "15%" }}
-            >
-            {loading ? 'Submitting...' : 'Submit Record'}
-        </LoadingButton>
+      ) : (
+        <Typography variant="h7" sx={{ marginTop: "20px" }}>
+          No Student Assigned yet
+        </Typography>
+      )}
     </Box>
   );
-}
+};
 
-export default RecordStudentResult
+export default LeaveRequests;
