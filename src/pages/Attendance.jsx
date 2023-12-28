a7162dc4c600cf0065dbf871495993c27fe94836
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 13:01:21 +0100

    code reformatting

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import Calendar from '../components/Calendar'
+import React from "react";
+import Calendar from "../components/Calendar";
 
 const Attendance = () => {
-  return (
-    <Calendar />
-  )
-}
+  return <Calendar />;
+};
 
-export default Attendance
-
+export default Attendance;
