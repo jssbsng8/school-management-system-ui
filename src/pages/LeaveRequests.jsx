6757666fa4f7a4ef8bf3c96f8879e76928840d30
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 03:07:01 +0100

    added exports to some tables, added leave requests

---
+++ src/pages/LeaveRequests.jsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import TableEditable from "../components/TableEditable";
+import { myData, myColumns } from "../data/leaveRequests";
+
+const LeaveRequests = () => {
+  return (
+    <Box sx={{ pt: "80px", pb: "20px" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: "16px",
+        }}
+      >
+        <Typography variant="h6">Leave Requests</Typography>
+      </Box>
+      {
+      myData ? (
+        <TableEditable 
+            myData={myData}
+            myColumns={myColumns}
+        />
+      ) : (
+        <Typography variant="h6">No Leave Request(s)</Typography>
+      )
+      }
+    </Box>
+  );
+};
+
+export default LeaveRequests;
