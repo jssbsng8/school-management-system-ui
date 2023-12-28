2799e5c76b72a9f84fa4d3de7057d51580e3371e
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:57:12 +0100

    code reformatting

--- src/data/takeAttendance.js
+++ src/data/takeAttendance.js
@@ -1,127 +1,122 @@
-import {
-    randomTraderName,
-    randomId
-  } from '@mui/x-data-grid-generator';
+import { randomTraderName, randomId } from "@mui/x-data-grid-generator";
 
 export const columns = [
-    { 
-        field: 'name', 
-        headerName: 'Name', 
-        width: 300, 
-        editable: false 
+  {
+    field: "name",
+    headerName: "Name",
+    width: 300,
+    editable: false,
+  },
+  {
+    field: "registration_number",
+    headerName: "Registration Number",
+    editable: false,
+    width: 250,
+    align: "left",
+    headerAlign: "left",
+  },
+  {
+    field: "attendance",
+    headerName: "Attendance",
+    type: "singleSelect",
+    width: 200,
+    align: "left",
+    valueOptions: ["PRESENT", "ABSENT", "LATE", "HOLIDAY", "LEAVE"],
+    editable: true,
+    renderCell: (params) => {
+      const cellValue = params.value;
+      let cellColor = "";
+
+      if (cellValue === "ABSENT") {
+        cellColor = "red";
+      } else if (cellValue === "PRESENT") {
+        cellColor = "green";
+      } else if (cellValue === "LATE") {
+        cellColor = "#ff9966";
+      }
+
+      return <div style={{ color: cellColor }}>{cellValue}</div>;
     },
-    {
-        field: 'registration_number',
-        headerName: 'Registration Number',
-        editable: false,
-        width: 250,
-        align: 'left',
-        headerAlign: 'left',
-    },
-    {
-        field: 'attendance',
-        headerName: 'Attendance',
-        type: 'singleSelect',
-        width: 200,
-        align: 'left',
-        valueOptions: ['PRESENT', 'ABSENT', 'LATE', 'HOLIDAY', 'LEAVE'],
-        editable: true,
-        renderCell: (params) => {
-            const cellValue = params.value;
-            let cellColor = '';
-        
-            if (cellValue === 'ABSENT') {
-              cellColor = 'red';
-            }
-            else if (cellValue === 'PRESENT'){
-                cellColor = 'green'
-            }
-            else if (cellValue === 'LATE'){
-                cellColor = '#ff9966'
-            }
-        
-            return <div style={{ color: cellColor }}>{cellValue}</div>;
-        },
-    },
-    {
-        field: 'time',
-        headerName: 'Time',
-        editable: false,
-        width: 200,
-        align: 'left',
-        headerAlign: 'left',
-    },
-  ];
-  
-  export const rows = [
-    {
-        id: 1,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 2,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 3,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 4,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 5,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 6,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 7,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 8,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 9,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
-    {
-        id: 10,
-        name: randomTraderName(),
-        registration_number: randomId().slice(0,8),
-        attendance: "ABSENT",
-        time: new Date().toLocaleTimeString(), 
-    },
+  },
+  {
+    field: "time",
+    headerName: "Time",
+    editable: false,
+    width: 200,
+    align: "left",
+    headerAlign: "left",
+  },
+];
+
+export const rows = [
+  {
+    id: 1,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 2,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 3,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 4,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 5,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 6,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 7,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 8,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 9,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
+  {
+    id: 10,
+    name: randomTraderName(),
+    registration_number: randomId().slice(0, 8),
+    attendance: "ABSENT",
+    time: new Date().toLocaleTimeString(),
+  },
 ];
