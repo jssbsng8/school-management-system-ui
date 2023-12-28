b49b9b8e834a2c690fd906b58f4253013c2110a4
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 22:40:29 +0100

    added export pdf pto custom material-react-table

--- src/data/teachersReviewsData.js
+++ src/data/teachersReviewsData.js
@@ -2,6 +2,11 @@ import { Rating } from "@mui/material";
 import { Avatar } from "@mui/material";
 export const teachersReviewColumn = [
   {
+    accessorKey: "id",
+    header: "Id",
+    size: 50,
+  },
+  {
     accessorKey: "img", //access nested data with dot notation
     header: "Image",
     size: 100,
