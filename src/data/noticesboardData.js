eda9e0df3e0679e9446b116172e08c58b01e66a4
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:56:51 +0100

    code reformatting

--- src/data/noticesboardData.js
+++ src/data/noticesboardData.js
@@ -1,20 +1,22 @@
 export const notices = [
-    {
-      id: 1,
-      title: "Important Announcement 1",
-      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      publish_date: "04/18/2023"
-    },
-    {
-      id: 2,
-      title: "Upcoming Event",
-      message: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      publish_date: "04/18/2023"
-    },
-    {
-      id: 3,
-      title: "Reminder: Submit Assignment",
-      message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
-      publish_date: "04/18/2023"
-    },
+  {
+    id: 1,
+    title: "Important Announcement 1",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    publish_date: "04/18/2023",
+  },
+  {
+    id: 2,
+    title: "Upcoming Event",
+    message:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    publish_date: "04/18/2023",
+  },
+  {
+    id: 3,
+    title: "Reminder: Submit Assignment",
+    message:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+    publish_date: "04/18/2023",
+  },
 ];
