2c61b97f8f64b1121ef1d7dadb759c311140a6c8
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:57:09 +0100

    code reformatting

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -13,66 +13,65 @@ export const subjectColumns = [
   },
 ];
 
-
 export const subjects = [
-    {
-      id: 1,
-      title: "English Language",
-      code: "GNS",
-      last_score: "72%",
-    },
-    {
-      id: 8,
-      title: "Mathematics",
-      code: "MTH",
-      last_score: "78%",
-    },
-    {
-      id: 3,
-      title: "Geography",
-      code: "GEO",
-      last_score: "50%",
-    },
-    {
-      id: 7,
-      title: "Civic Education",
-      code: "CVE",
-      last_score: "49%",
-    },
-    {
-      id: 5,
-      title: "History",
-      code: "HST",
-      last_score: "40%",
-    },
-    {
-      id: 4,
-      title: "Social Studies",
-      code: "SOS",
-      last_score: "40%",
-    },
-    {
-      id: 7,
-      title: "Computer Science",
-      code: "CSC",
-      last_score: "40%",
-    },
-    {
-      id: 8,
-      title: "Agricultural Science",
-      code: "AGE",
-      last_score: "40%",
-    },
-    {
-      id: 9,
-      title: "Further Mathematics",
-      code: "FMTH",
-      last_score: "40%",
-    },
-    {
-      id: 10,
-      title: "Geography",
-      code: "GEO",
-      last_score: "40%",
-    },
-  ];
+  {
+    id: 1,
+    title: "English Language",
+    code: "GNS",
+    last_score: "72%",
+  },
+  {
+    id: 8,
+    title: "Mathematics",
+    code: "MTH",
+    last_score: "78%",
+  },
+  {
+    id: 3,
+    title: "Geography",
+    code: "GEO",
+    last_score: "50%",
+  },
+  {
+    id: 7,
+    title: "Civic Education",
+    code: "CVE",
+    last_score: "49%",
+  },
+  {
+    id: 5,
+    title: "History",
+    code: "HST",
+    last_score: "40%",
+  },
+  {
+    id: 4,
+    title: "Social Studies",
+    code: "SOS",
+    last_score: "40%",
+  },
+  {
+    id: 7,
+    title: "Computer Science",
+    code: "CSC",
+    last_score: "40%",
+  },
+  {
+    id: 8,
+    title: "Agricultural Science",
+    code: "AGE",
+    last_score: "40%",
+  },
+  {
+    id: 9,
+    title: "Further Mathematics",
+    code: "FMTH",
+    last_score: "40%",
+  },
+  {
+    id: 10,
+    title: "Geography",
+    code: "GEO",
+    last_score: "40%",
+  },
+];
