838221b141c96892e55af044e1904a4953cc077b
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:47:40 +0100

    code refactoring and tableEditable

--- src/components/utils/toastUtils.js
+++ src/components/utils/toastUtils.js
@@ -1,10 +1,10 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Utility function to show success message
 export const successToast = (message) => {
   toast.success(message, {
-    position: 'top-right',
+    position: "top-right",
     autoClose: 3000,
     hideProgressBar: false,
     closeOnClick: true,
@@ -16,7 +16,7 @@ export const successToast = (message) => {
 // Utility function to show error message
 export const errorToast = (message) => {
   toast.error(message, {
-    position: 'top-right',
+    position: "top-right",
     autoClose: 3000,
     hideProgressBar: false,
     closeOnClick: true,
@@ -27,12 +27,12 @@ export const errorToast = (message) => {
 
 // Utility function to show error message
 export const warningToast = (message) => {
-    toast.warning(message, {
-      position: 'top-right',
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-    });
-  };
+  toast.warning(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
