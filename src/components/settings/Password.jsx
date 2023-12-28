d0b66082ae3a7254bd7c6a666c7c4aefaed2b8ef
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:56:13 +0100

    code reformatting

--- src/components/settings/Password.jsx
+++ src/components/settings/Password.jsx
@@ -1,27 +1,26 @@
 import { Divider, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
-import LoadingButton from '@mui/lab/LoadingButton';
+import LoadingButton from "@mui/lab/LoadingButton";
 import { successToast, errorToast } from "../utils/toastUtils";
 
 const Password = () => {
   const [loading, setLoading] = useState(false);
 
   const handlePasswordChange = async () => {
-    setLoading(true)
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // function to change user password
     const passwordChange = true;
-    if (passwordChange){
-      successToast("Password updated succesfully")
-      setLoading(false)
+    if (passwordChange) {
+      successToast("Password updated succesfully");
+      setLoading(false);
+    } else {
+      errorToast("Failed to update profile");
+      setLoading(false);
     }
-    else{
-      errorToast("Failed to update profile")
-      setLoading(false)
-    }
-  }
+  };
   return (
     <Box>
       <Typography variant="subtitle1">Password Reset</Typography>
@@ -62,8 +61,8 @@ const Password = () => {
         </Box>
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
+            display: "flex",
+            justifyContent: "flex-end",
             mt: 3,
           }}
         >
@@ -75,7 +74,7 @@ const Password = () => {
             onClick={handlePasswordChange}
             sx={{ width: "200px" }}
           >
-            {loading ? 'Please Wait...' : 'Change Password'}
+            {loading ? "Please Wait..." : "Change Password"}
           </LoadingButton>
         </Box>
       </Box>
