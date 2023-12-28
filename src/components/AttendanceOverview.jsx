b80e429bd2b9640d297832c5beff65ea7cac7738
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:56:02 +0100

    code reformatting

--- src/components/AttendanceOverview.jsx
+++ src/components/AttendanceOverview.jsx
@@ -3,24 +3,30 @@ import { Box, Typography, LinearProgress } from "@mui/material";
 
 const AttendanceOverview = ({ title, data, progress }) => {
   return (
-    <Box sx={{
+    <Box
+      sx={{
         display: "flex",
         flexDirection: "column",
         padding: "16px",
-    }}>
-        <Box
-            sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-            }}
-        >
-            <Typography variant="subtitle1">{title}</Typography>
-            <Typography variant="subtitle1" sx={{ textAlign: "right"}}>{data}</Typography>
-        </Box>
-      <Box sx={{
-        width: "100%",
-      }}>
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="subtitle1">{title}</Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "right" }}>
+          {data}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+        }}
+      >
         <LinearProgress
           variant="determinate"
           value={progress}
@@ -33,4 +39,4 @@ const AttendanceOverview = ({ title, data, progress }) => {
   );
 };
 
-export default AttendanceOverview
+export default AttendanceOverview;
