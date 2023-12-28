d814a45f9abf23bd2f4d3bc0b3901ce4ee5ee884
ademic2022 <adeeyomichael77@gmail.com>
2023-12-28 12:56:33 +0100

    code reformatting

--- src/data/classrooms.js
+++ src/data/classrooms.js
@@ -1,37 +1,35 @@
 export const classroomColumns = [
-    {
-      accessorKey: "title", //access nested data with dot notation
-      header: "Title",
-    },
-    {
-      accessorKey: "code",
-      header: "Code",
-    },
-    {
-      accessorKey: "stream",
-      header: "Stream",
-    },
-    {
-        accessorKey: "capacity",
-        header: "Capacity",
-      },
-  ];
-  
-  
-  export const classrooms = [
-      {
-        id: 1,
-        title: "Junior Secondary School 3",
-        code: "JSS 3",
-        capacity: 100,
-        stream: "B",
-      },
-      {
-        id: 8,
-        title: "Junior Secondary School 1",
-        code: "MTH",
-        capacity: 150,
-        stream: "A",
-      },
-    ];
-  
+  {
+    accessorKey: "title", //access nested data with dot notation
+    header: "Title",
+  },
+  {
+    accessorKey: "code",
+    header: "Code",
+  },
+  {
+    accessorKey: "stream",
+    header: "Stream",
+  },
+  {
+    accessorKey: "capacity",
+    header: "Capacity",
+  },
+];
+
+export const classrooms = [
+  {
+    id: 1,
+    title: "Junior Secondary School 3",
+    code: "JSS 3",
+    capacity: 100,
+    stream: "B",
+  },
+  {
+    id: 8,
+    title: "Junior Secondary School 1",
+    code: "MTH",
+    capacity: 150,
+    stream: "A",
+  },
+];
