bc8966f5ae68ac5930d328d5b3828dd5cda854c9
ademic2022 <adeeyomichael77@gmail.com>
2023-12-29 12:58:57 +0100

    student/teacher profile created, table modified...

--- src/data/leaveData.js
+++ src/data/leaveData.js
@@ -1,114 +1,83 @@
+// eslint-disable-next-line
+import { randomCreatedDate } from "@mui/x-data-grid-generator";
+
 export const leaveColumn = [
   {
-    accessorKey: "id",
-    header: "Id",
-    size: 50,
+    field: "name",
+    headerName: "Name",
+    width: 180,
+    editable: true,
   },
   {
-    accessorKey: "apply_date",
-    header: "Apply Date",
+    field: "apply_date",
+    headerName: "Apply Date",
+    // type: "date",
+    width: 130,
+    align: "left",
+    editable: false,
   },
   {
-    accessorKey: "from",
-    header: "From",
+    field: "from",
+    headerName: "From",
+    width: 130,
+    editable: true,
+    type: "date",
   },
   {
-    accessorKey: "to",
-    header: "To",
+    field: "to",
+    headerName: "To",
+    width: 130,
+    editable: true,
+    type: "date",
   },
   {
-    accessorKey: "reason",
-    header: "Reason",
+    field: "reason",
+    headerName: "Reason",
+    width: 180,
+    editable: true,
   },
   {
-    accessorKey: "status",
-    header: "Status",
-    Cell: ({ cell, row }) => (
-      <div>
-        {row.original.status === "approved" && (
-          <span
-            className="status"
-            style={{ color: "#388b84", backgroundColor: "#388b8433" }}
-          >
-            {cell.getValue()}
-          </span>
-        )}
-        {row.original.status === "pending" && (
-          <span
-            className="status"
-            style={{ color: "#ff9966", backgroundColor: "#ff996633" }}
-          >
-            {cell.getValue()}
-          </span>
-        )}
-        {row.original.status === "cancelled" && (
-          <span
-            className="status"
-            style={{ color: "#fd4332", backgroundColor: "#fd433233" }}
-          >
-            {cell.getValue()}
-          </span>
-        )}
-      </div>
-    ),
+    field: "status",
+    headerName: "Status",
+    width: 100,
+    editable: false,
+    type: "singleSelect",
+    valueOptions: ["Approved", "Pending", "Rejected"],
+    align: "left",
+    headerAlign: "left",
+    renderCell: (params) => {
+      const cellValue = params.value;
+      let cellColor = "";
+
+      if (cellValue === "Rejected") {
+        cellColor = "red";
+      } else if (cellValue === "Approved") {
+        cellColor = "green";
+      } else if (cellValue === "Pending") {
+        cellColor = "#ff9966";
+      }
+
+      return <div style={{ color: cellColor }}>{cellValue}</div>;
+    },
+  },
+  {
+    field: "approved_by",
+    headerName: "Approved By",
+    width: 160,
+    editable: false,
   },
 ];
 
+// MyData.js
 export const leaveData = [
-  {
-    id: 1,
-    apply_date: "2023-01-15",
-    from: "2023-01-10",
-    to: "2023-01-19",
-    reason: "Fever",
-    status: "approved",
-  },
-  {
-    id: 2,
-    apply_date: "2023-02-20",
-    from: "2023-02-15",
-    to: "2023-02-18",
-    reason: "Family Function",
-    status: "cancelled",
-  },
-  {
-    id: 3,
-    apply_date: "2023-03-10",
-    from: "2023-03-05",
-    to: "2023-03-12",
-    reason: "Emergency",
-    status: "pending",
-  },
-  {
-    id: 4,
-    apply_date: "2023-04-22",
-    from: "2023-04-15",
-    to: "2023-04-22",
-    reason: "Financial Breakdown",
-    status: "approved",
-  },
-  {
-    id: 5,
-    apply_date: "2023-05-12",
-    from: "2023-05-05",
-    to: "2023-05-12",
-    reason: "",
-    status: "pending",
-  },
-  {
-    id: 6,
-    apply_date: "2023-06-24",
-    from: "2023-06-20",
-    to: "2023-06-24",
-    reason: "Miscellaneous",
-    status: "cancelled",
-  },
-  {
-    id: 7,
-    apply_date: "2023-07-21",
-    from: "2023-07-15",
-    to: "2023-07-21",
-    reason: "Miscellaneous",
-    status: "approved",
-  },
+  // {
+  //   id: 1,
+  //   name: "John Smith",
+  //   apply_date: "2023-04-22",
+  //   from: randomCreatedDate(),
+  //   to: randomCreatedDate(),
+  //   reason: "Financial Breakdown",
+  //   status: "Pending",
+  //   approved_by: "",
+  // },
 ];
