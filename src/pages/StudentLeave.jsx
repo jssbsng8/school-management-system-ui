b501e0e8cb94ba7a94ca4e420cc8043b484d963e
ademic2022 <adeeyomichael77@gmail.com>
2023-12-29 13:02:41 +0100

    student/teacher profile created, table modified...

--- src/pages/StudentLeave.jsx
+++ src/pages/StudentLeave.jsx
@@ -1,11 +1,68 @@
-import { Box, Button, Typography } from "@mui/material";
-import React from "react";
-import { FiPlus } from "react-icons/fi";
-import { Link } from "react-router-dom";
-import Table from "../components/Table";
-import { leaveData, leaveColumn } from "../data/leaveData";
+// import { Box, Button, Typography } from "@mui/material";
+// import React from "react";
+// import { FiPlus } from "react-icons/fi";
+// import { Link } from "react-router-dom";
+// import Table from "../components/Table";
+// import { leaveData, leaveColumn } from "../data/leaveData";
 
-const StudentLeave = () => {
+// const StudentLeave = () => {
+//   return (
+//     <Box sx={{ pt: "80px", pb: "20px" }}>
+//       <Box
+//         sx={{
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "space-between",
+//           marginBottom: "16px",
+//         }}
+//       >
+//         <Typography variant="h6">Leave of Absent</Typography>
+//         <Link to="/leave/apply" style={{ textDecoration: "none" }}>
+//           <Button
+//             variant="contained"
+//             color="primary"
+//             startIcon={<FiPlus />}
+//             sx={{ borderRadius: "20px" }}
+//           >
+//             Apply Here
+//           </Button>
+//         </Link>
+//       </Box>
+//       <Table
+//         data={leaveData}
+//         fields={leaveColumn}
+//         numberOfRows={leaveData.length}
+//         enableTopToolBar={true}
+//         enableBottomToolBar={true}
+//         enablePagination={true}
+//         enableRowSelection={false}
+//         enableColumnFilters={true}
+//         enableEditing={false}
+//         enableColumnDragging={true}
+//         showPreview={false}
+//         routeLink="leave"
+//       />
+//     </Box>
+//   );
+// };
+
+// export default StudentLeave;
+
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import TableEditable from "../components/TableEditable";
+import { leaveData, leaveColumn } from "../data/leaveData";
+import { useUser } from "../components/utils/userContext";
+
+const LeaveRequests = () => {
+  const { user } = useUser()
+  
+  const currentTime = new Date().toLocaleDateString();
+  const defaultLeaveValues = {
+    name: `${user.first_name} ${user.last_name}`,
+    apply_date: currentTime,
+    status: "Pending",
+  };
   return (
     <Box sx={{ pt: "80px", pb: "20px" }}>
       <Box
@@ -16,34 +73,258 @@ const StudentLeave = () => {
           marginBottom: "16px",
         }}
       >
-        <Typography variant="h6">Leave of Absent</Typography>
-        <Link to="/leave/apply" style={{ textDecoration: "none" }}>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<FiPlus />}
-            sx={{ borderRadius: "20px" }}
-          >
-            Apply Here
-          </Button>
-        </Link>
+        <Typography variant="h6">Apply For Leave</Typography>
       </Box>
-      <Table
-        data={leaveData}
-        fields={leaveColumn}
-        numberOfRows={leaveData.length}
-        enableTopToolBar={true}
-        enableBottomToolBar={true}
-        enablePagination={true}
-        enableRowSelection={false}
-        enableColumnFilters={true}
-        enableEditing={false}
-        enableColumnDragging={true}
-        showPreview={false}
-        routeLink="leave"
-      />
+      {leaveData ? (
+        <TableEditable
+          myData={leaveData}
+          myColumns={leaveColumn}
+          enableSubmitButton={true}
+          enableAddNewRow={true}
+          Decision={null}
+          defaultValues={defaultLeaveValues}
+        />
+      ) : (
+        <Typography variant="h6">No Leave Request(s)</Typography>
+      )}
     </Box>
   );
 };
 
-export default StudentLeave;
+export default LeaveRequests;
+
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import AddIcon from '@mui/icons-material/Add';
+// import EditIcon from '@mui/icons-material/Edit';
+// import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+// import SaveIcon from '@mui/icons-material/Save';
+// import CancelIcon from '@mui/icons-material/Close';
+// import {
+//   GridRowModes,
+//   DataGrid,
+//   GridToolbarContainer,
+//   GridActionsCellItem,
+//   GridRowEditStopReasons,
+// } from '@mui/x-data-grid';
+// import {
+//   randomCreatedDate,
+//   randomTraderName,
+//   randomId,
+//   randomArrayItem,
+// } from '@mui/x-data-grid-generator';
+
+// const roles = ['Market', 'Finance', 'Development'];
+// const randomRole = () => {
+//   return randomArrayItem(roles);
+// };
+
+// const initialRows = [
+//   {
+//     id: randomId(),
+//     name: randomTraderName(),
+//     age: 25,
+//     joinDate: randomCreatedDate(),
+//     role: randomRole(),
+//   },
+//   {
+//     id: randomId(),
+//     name: randomTraderName(),
+//     age: 36,
+//     joinDate: randomCreatedDate(),
+//     role: randomRole(),
+//   },
+//   {
+//     id: randomId(),
+//     name: randomTraderName(),
+//     age: 19,
+//     joinDate: randomCreatedDate(),
+//     role: randomRole(),
+//   },
+//   {
+//     id: randomId(),
+//     name: randomTraderName(),
+//     age: 28,
+//     joinDate: randomCreatedDate(),
+//     role: randomRole(),
+//   },
+//   {
+//     id: randomId(),
+//     name: randomTraderName(),
+//     age: 23,
+//     joinDate: randomCreatedDate(),
+//     role: randomRole(),
+//   },
+// ];
+
+// function EditToolbar(props) {
+//   const { setRows, setRowModesModel } = props;
+
+//   const handleClick = () => {
+//     const id = randomId();
+//     setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
+//     setRowModesModel((oldModel) => ({
+//       ...oldModel,
+//       [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+//     }));
+//   };
+
+//   return (
+//     <GridToolbarContainer>
+//       <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+//         Add record
+//       </Button>
+//     </GridToolbarContainer>
+//   );
+// }
+
+// export default function FullFeaturedCrudGrid() {
+//   const [rows, setRows] = React.useState(initialRows);
+//   const [rowModesModel, setRowModesModel] = React.useState({});
+
+//   const handleRowEditStop = (params, event) => {
+//     if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+//       event.defaultMuiPrevented = true;
+//     }
+//   };
+
+//   const handleEditClick = (id) => () => {
+//     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+//   };
+
+//   const handleSaveClick = (id) => () => {
+//     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+//   };
+
+//   const handleDeleteClick = (id) => () => {
+//     setRows(rows.filter((row) => row.id !== id));
+//   };
+
+//   const handleCancelClick = (id) => () => {
+//     setRowModesModel({
+//       ...rowModesModel,
+//       [id]: { mode: GridRowModes.View, ignoreModifications: true },
+//     });
+
+//     const editedRow = rows.find((row) => row.id === id);
+//     if (editedRow.isNew) {
+//       setRows(rows.filter((row) => row.id !== id));
+//     }
+//   };
+
+//   const processRowUpdate = (newRow) => {
+//     const updatedRow = { ...newRow, isNew: false };
+//     setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+//     return updatedRow;
+//   };
+
+//   const handleRowModesModelChange = (newRowModesModel) => {
+//     setRowModesModel(newRowModesModel);
+//   };
+
+//   const columns = [
+//     { field: 'name', headerName: 'Name', width: 180, editable: true },
+//     {
+//       field: 'age',
+//       headerName: 'Age',
+//       type: 'number',
+//       width: 80,
+//       align: 'left',
+//       headerAlign: 'left',
+//       editable: true,
+//     },
+//     {
+//       field: 'joinDate',
+//       headerName: 'Join date',
+//       // type: 'date',
+//       width: 180,
+//       editable: true,
+//     },
+//     {
+//       field: 'role',
+//       headerName: 'Department',
+//       width: 220,
+//       editable: true,
+//       type: 'singleSelect',
+//       valueOptions: ['Market', 'Finance', 'Development'],
+//     },
+//     {
+//       field: 'actions',
+//       type: 'actions',
+//       headerName: 'Actions',
+//       width: 100,
+//       cellClassName: 'actions',
+//       getActions: ({ id }) => {
+//         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+//         if (isInEditMode) {
+//           return [
+//             <GridActionsCellItem
+//               icon={<SaveIcon />}
+//               label="Save"
+//               sx={{
+//                 color: 'primary.main',
+//               }}
+//               onClick={handleSaveClick(id)}
+//             />,
+//             <GridActionsCellItem
+//               icon={<CancelIcon />}
+//               label="Cancel"
+//               className="textPrimary"
+//               onClick={handleCancelClick(id)}
+//               color="inherit"
+//             />,
+//           ];
+//         }
+
+//         return [
+//           <GridActionsCellItem
+//             icon={<EditIcon />}
+//             label="Edit"
+//             className="textPrimary"
+//             onClick={handleEditClick(id)}
+//             color="inherit"
+//           />,
+//           <GridActionsCellItem
+//             icon={<DeleteIcon />}
+//             label="Delete"
+//             onClick={handleDeleteClick(id)}
+//             color="inherit"
+//           />,
+//         ];
+//       },
+//     },
+//   ];
+
+//   return (
+//     <Box
+//       sx={{
+//         height: 500,
+//         width: '100%',
+//         '& .actions': {
+//           color: 'text.secondary',
+//         },
+//         '& .textPrimary': {
+//           color: 'text.primary',
+//         },
+//       }}
+//     >
+//       <DataGrid
+//         rows={rows}
+//         columns={columns}
+//         editMode="row"
+//         rowModesModel={rowModesModel}
+//         onRowModesModelChange={handleRowModesModelChange}
+//         onRowEditStop={handleRowEditStop}
+//         processRowUpdate={processRowUpdate}
+//         slots={{
+//           toolbar: EditToolbar,
+//         }}
+//         slotProps={{
+//           toolbar: { setRows, setRowModesModel },
+//         }}
+//       />
+//     </Box>
+//   );
+// }
