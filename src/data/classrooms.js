a91f70ec4214540b94b7ed03c41ea1b19da65970
ademic2022 <adeeyomichael77@gmail.com>
2023-12-29 21:05:03 +0100

    admin general settings, modified grid table...

--- src/data/classrooms.js
+++ src/data/classrooms.js
@@ -22,18 +22,65 @@ export const classroomColumns = [
   },
 ];
 
+export const editClassroomColumn = [
+  {
+    field: "id",
+    headerName: "Id",
+    width: 100,
+    editable: true,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 350,
+    align: "left",
+    editable: true,
+    type: "singleSelect",
+    valueOptions: [
+      "JUNIOR SECONDARY SCHOOL 1",
+      "JUNIOR SECONDARY SCHOOL 2",
+      "JUNIOR SECONDARY SCHOOL 3",
+      "SENIOR SECONDARY SCHOOL 1",
+      "SENIOR SECONDARY SCHOOL 2",
+      "SENIOR SECONDARY SCHOOL 3",
+    ],
+  },
+  {
+    field: "code",
+    headerName: "Code",
+    width: 130,
+    editable: true,
+    type: "singleSelect",
+    valueOptions: ["JSS 1", "JSS 2", "JSS 3", "SSS 1", "SSS 2", "SSS 3"],
+  },
+  {
+    field: "capacity",
+    headerName: "Capacity",
+    width: 130,
+    editable: true,
+    align: "left",
+    type: "number",
+  },
+  {
+    field: "stream",
+    headerName: "Stream",
+    width: 180,
+    editable: true,
+  },
+];
+
 export const classrooms = [
   {
     id: 1,
-    title: "Junior Secondary School 3",
-    code: "JSS 3",
+    title: "JUNIOR SECONDARY SCHOOL 1",
+    code: "JSS 1",
     capacity: 100,
     stream: "B",
   },
   {
     id: 8,
-    title: "Junior Secondary School 1",
-    code: "MTH",
+    title: "JUNIOR SECONDARY SCHOOL 3",
+    code: "JSS 3",
     capacity: 150,
     stream: "A",
   },
