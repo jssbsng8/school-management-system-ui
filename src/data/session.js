7889e378bc9dcc13868d37db320d4cbc4a2dcda4
ademic2022 <adeeyomichael77@gmail.com>
2023-12-29 21:05:49 +0100

    admin general settings, modified grid table...

---
+++ src/data/session.js
@@ -0,0 +1,26 @@
+export const sessionColumn = [
+  {
+    field: "id",
+    headerName: "Id",
+    width: 100,
+    editable: true,
+  },
+  {
+    field: "name",
+    headerName: "Title",
+    width: 350,
+    align: "left",
+    editable: true,
+  },
+];
+
+export const session = [
+  {
+    id: 1,
+    name: "FIRST TERM",
+  },
+  {
+    id: 2,
+    name: "SECOND TERM",
+  },
+];
