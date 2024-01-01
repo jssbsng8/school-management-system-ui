c69f0c679cee295bd31053971d00761a3cbb30cc
ademic2022 <adeeyomichael77@gmail.com>
2024-01-01 16:45:46 +0100

    modified grid table for crud operation

--- src/components/generalSettings/Classrooms.jsx
+++ src/components/generalSettings/Classrooms.jsx
@@ -1,31 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography, Paper, Toolbar } from "@mui/material";
 import TableEditable from "../TableEditable";
-import { classrooms, editClassroomColumn } from "../../data/classrooms";
+import { editClassroomColumn } from "../../data/classrooms";
+import { successToast, errorToast } from "../utils/toastUtils";
+import {
+  getFetchedData,
+  postData,
+  patchData,
+  deleteData,
+} from "../../apiCalls/authApi";
+import { CORE } from "../../apiCalls/endpoints";
 
 const Classrooms = () => {
+  const [classroomData, setClassroomData] = useState();
+
+  useEffect(() => {
+    const classroomDataFromApi = async () => {
+      try {
+        const fetchedData = await getFetchedData(CORE.CLASSROOM);
+        // setClassroomData((prevData) => [...prevData, ...fetchedData]);
+        setClassroomData(fetchedData);
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    };
+
+    classroomDataFromApi();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleNewRowSave = async (newClassroom) => {
+    if (newClassroom.isNew) {
+      // handle new row
+      try {
+        const successful = await postData(CORE.CLASSROOM, newClassroom);
+        if (successful) {
+          successToast("New row added!");
+        }
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    } else {
+      // handle update row
+      try {
+        if (Object.keys(newClassroom.updatedFields).length !== 0) {
+          const successful = await patchData(
+            CORE.GET_CLASSROOM(newClassroom.id),
+            newClassroom.updatedFields
+          );
+          if (successful) {
+            successToast("Data Updated!");
+          }
+        } else {
+          errorToast("No changes detected!");
+        }
+      } catch (error) {
+        console.error("Error saving/updating classroom data:", error.message);
+      }
+    }
+  };
+  const handleDeleteRow = async (classroomId) => {
+    try {
+      const successful = await deleteData(CORE.GET_CLASSROOM(classroomId));
+      if (successful) {
+        successToast("Classroom deleted!");
+      }
+    } catch (error) {
+      console.error("Error deleting classroom:", error.message);
+      errorToast("Error deleting classroom");
+    }
+  };
+
   return (
     <Box
       component="main"
       sx={{ flexGrow: 1, bgcolor: "background.default", pb: "20px" }}
     >
-      <Paper sx={{mb:1}}>
+      <Paper sx={{ mb: 1 }}>
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
             Add/Edit Classroom
           </Typography>
         </Toolbar>
       </Paper>
-      {classrooms ? (
+      {classroomData ? (
         <TableEditable
-          myData={classrooms}
+          myData={classroomData}
           myColumns={editClassroomColumn}
-          enableSubmitButton={true}
+          enableSubmitButton={false}
           enableAddNewRow={true}
-          Decision={"John Cena"}
+          onNewRowSave={handleNewRowSave}
+          onDeleteRow={handleDeleteRow}
+          Decision={false}
         />
       ) : (
-        <Typography variant="h6">No Leave Request(s)</Typography>
+        <Typography variant="h6">Loading classroom data...</Typography>
       )}
     </Box>
   );
