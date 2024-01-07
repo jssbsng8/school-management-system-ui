3a0190e1a26907234d6880a47b430fb7d6e4ae76
ademic2022 <adeeyomichael77@gmail.com>
2024-01-07 01:33:21 +0100

    admin development

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -57,11 +57,11 @@ const RegisterForm = ({ onSubmit, onToggleForm }) => {
           })
           console.log(JSON.stringify(validated));
           if(response.ok){
-              const message = 'Registration Successful!, check your email for activation link'
-              successToast(message);
-              await new Promise(resolve => setTimeout(resolve, 3000));
-              setLoading(false);
-              navigate("/success");
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            setLoading(false);
+            navigate("/success");
+            const message = 'Registration Successful!, check your email for activation link'
+            successToast(message);
           }else{
               const message = `Registration failed:, ${response.statusText}`
               errorToast(message);
