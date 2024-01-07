b2564cf00782be40cb96b8f5d2c14b6d41cc5171
ademic2022 <adeeyomichael77@gmail.com>
2024-01-07 01:36:40 +0100

    admin development

---
+++ src/data/teacherDetails.js
@@ -0,0 +1,98 @@
+export const editTeacherColumn = [
+    {
+      field: "id",
+      headerName: "Id",
+      width: 100,
+      editable: true,
+    },
+    {
+      field: "first_name",
+      headerName: "First Name",
+      width: 120,
+      align: "left",
+      editable: true,
+      // type: "singleSelect",
+      // valueOptions: ["John Smith", "Jane Doe", "Alice Williams", "Emily Davis"],
+    },
+    {
+      field: "last_name",
+      headerName: "Last Name",
+      width: 120,
+      align: "left",
+      editable: true,
+      // type: "singleSelect",
+      // valueOptions: ["John Smith", "Jane Doe", "Alice Williams", "Emily Davis"],
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      width: 350,
+      editable: true,
+    },
+  ];
+
+
+//   export const teachers = [
+    // {
+    //   id: 1,
+    //   first_name: "John",
+    //   last_name: "Smith",
+    // //   name: "John Smith",
+    //   email: "johnsmith@example.com",
+    //   address: "123 Main Street, New York, NY 10001",
+    //   subject: "English Language",
+    //   img: "/images/avatars/avatar1.png",
+    //   assigned_subjects: [
+    //     { title: "Mathematics", classroom: "Junior Secondary School 1" },
+    //     { title: "English Language", classroom: "Junior Secondary School 3" },
+    //   ],
+    
+    // {
+    //   id: 2,
+    //   first_name: "Jane",
+    //   last_name: "Doe",
+    // //   name: "Jane Doe",
+    //   email: "janedoe@example.com",
+    //   address: "456 Park Avenue, New York, NY 10022",
+    //   subject: "Mathematics",
+    //   img: "/images/avatars/avatar2.png",
+    //   assigned_subjects: [
+    //     { title: "Mathematics", classroom: "Junior Secondary School 1" },
+    //     { title: "English Language", classroom: "Junior Secondary School 3" },
+    //   ],
+    // },
+    // {
+    //   id: 3,
+    //   first_name: "Bob",
+    //   last_name: "Johnson",
+    // //   name: "Bob Johnson",
+    //   email: "bobjohnson@example.com",
+    //   address: "789 Broadway, New York, NY 10011",
+    //   subject: "Geography",
+    //   img: "/images/avatars/avatar3.png",
+    //   assigned_subjects: [
+    //     { title: "Mathematics", classroom: "Junior Secondary School 1" },
+    //     { title: "English Language", classroom: "Junior Secondary School 3" },
+    //   ],
+    // },
+    // {
+    //   id: 4,
+    //   first_name: "Alice",
+    //   last_name: "Williams",
+    // //   name: "Alice Williams",
+    //   email: "alicewilliams@example.com",
+    //   address: "321 5th Avenue, New York, NY 10016",
+    //   subject: "Economics",
+    //   img: "/images/avatars/avatar4.png",
+    //   assigned_subjects: [
+    //     { title: "Mathematics", classroom: "Junior Secondary School 1" },
+    //     { title: "English Language", classroom: "Junior Secondary School 3" },
+    //   ],
+    // },
+//   ];
