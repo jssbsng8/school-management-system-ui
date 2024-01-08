97ce6aa52ee8543db7074ee6fe80919b3b3d0a4b
“Ademic” <adeeyomichael77@gmail.com>
2024-01-08 17:38:52 +0100

    user profile image and thumbnail fixed

--- src/apiCalls/endpoints.js
+++ src/apiCalls/endpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = "http://127.0.0.1:8000";
 
 export const AUTH_ENDPOINTS = {
   LOGIN: `${BASE_URL}/auth/login/`,
@@ -7,7 +7,9 @@ export const AUTH_ENDPOINTS = {
 };
 export const USER_ENDPOINTS = {
   USER: `${BASE_URL}/auth/users/`,
+  UPLOAD_IMAGE: `${BASE_URL}/auth/profile-image/`,
   GET_OR_UPDATE_USER: (userId) => `${BASE_URL}/auth/users/${userId}/`,
+  PROFILE_IMAGE: (userId) => `${BASE_URL}/auth/profile-image/?user=${userId}`,
   ACTIVATION: `${BASE_URL}/authusers/activation/`,
   AUTHENTICATED_USER: `${BASE_URL}/authusers/me/`,
   RESEND_ACTIVATION: `${BASE_URL}/authusers/resend_activation/`,
@@ -23,13 +25,13 @@ export const CORE = {
   SUBJECT: `${BASE_URL}/core/subjects/`,
   FILTERED_SUBJECT: `${BASE_URL}/core/subjects/?class_room=10`,
   TEACHER: `${BASE_URL}/core/teachers`,
-  GET_ASSIGNED_SUBJECTS:  `${BASE_URL}/core/teachers/get_assigned_subjects/`,
-  GET_ENROLLED_SUBJECTS:  `${BASE_URL}/core/students/get_enrolled_subjects/`,
+  GET_ASSIGNED_SUBJECTS: `${BASE_URL}/core/teachers/get_assigned_subjects/`,
+  GET_ENROLLED_SUBJECTS: `${BASE_URL}/core/students/get_enrolled_subjects/`,
   GET_ADMIN: (adminId) => `${BASE_URL}/auth/users/${adminId}/`,
   GET_CLASSROOM: (classroomId) => `${BASE_URL}/core/classrooms/${classroomId}/`,
   GET_SUBJECT: (subjectId) => `${BASE_URL}/core/subjects/${subjectId}/`,
-  GET_FILTERED_SUBJECT: (classroomId) => `${BASE_URL}/core/subjects/?class_room=${classroomId}`,
+  GET_FILTERED_SUBJECT: (classroomId) =>
+    `${BASE_URL}/core/subjects/?class_room=${classroomId}`,
   GET_STUDENT: (studentId) => `${BASE_URL}/core/students/${studentId}/`,
   GET_TEACHER: (teacherId) => `${BASE_URL}/core/teachers/${teacherId}/`,
 };
-
