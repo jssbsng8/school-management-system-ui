30d049f7ea679c03ce1263b28b66494f5e71c1c9
“Ademic” <adeeyomichael77@gmail.com>
2024-01-08 07:39:00 +0100

    users page in progress...

---
+++ src/data/allUsers.js
@@ -0,0 +1,20 @@
+export const column = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "first_name", headerName: "First name", width: 150 },
+    { field: "last_name", headerName: "Last name", width: 100 },
+    {
+      field: "username",
+      headerName: "Username",
+      width: 100,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      width: 150      
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 200      
+    },
+  ];
