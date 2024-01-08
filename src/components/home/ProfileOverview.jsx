2941b5780ae6fdfaecc8a448721cec5abff71b5e
“Ademic” <adeeyomichael77@gmail.com>
2024-01-08 17:39:59 +0100

    user profile image and thumbnail fixed

--- src/components/home/ProfileOverview.jsx
+++ src/components/home/ProfileOverview.jsx
@@ -6,17 +6,14 @@ const ProfileOverview = ({ fullName, attendancePercentage, userName }) => {
   return (
     <Paper elevation={3} style={{ padding: 20 }}>
       <Grid container spacing={2} alignItems="center">
-        {/* Left column for the student image */}
         <Grid item xs={12} sm={4}>
-          {/* Add your student image component or placeholder here */}
           <img
-            src="/images/avatars/profile-avatar.png" // Replace with the actual image source
+            src={localStorage.getItem("userImage")}
             alt="Student"
             style={{ width: "100%", borderRadius: "50%" }}
           />
         </Grid>
 
-        {/* Right column for the welcome message */}
         {role === "Student" && (
           <Grid item xs={12} sm={8}>
             <Typography variant="h4" gutterBottom>
