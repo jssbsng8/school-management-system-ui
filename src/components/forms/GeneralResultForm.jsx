a6ffcdff985c33f2aadd3d4dcea497ae7c8e0385
“Ademic” <adeeyomichael77@gmail.com>
2024-01-09 22:31:09 +0100

    Refactor: Code Refactoring

--- src/components/forms/GeneralResultForm.jsx
+++ src/components/forms/GeneralResultForm.jsx
@@ -7,6 +7,7 @@ import {
   MenuItem,
   Select,
   Box,
+  Grid,
 } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 
@@ -27,51 +28,55 @@ const GeneralResultForm = ({ onSubmit, loading }) => {
     onSubmit(selectedSession, selectedClassroom);
   };
   return (
-    <Paper elevation={3} style={{ padding: 20 }}>
-      <Typography variant="h6" gutterBottom>
-        Check Result
-      </Typography>
-      <Box sx={{ width: "100%", margin: "auto" }}>
-        <FormControl sx={{ m: 1, minWidth: "100%" }}>
-          <InputLabel id="session-label">Session</InputLabel>
-          <Select
-            labelId="session-label"
-            id="session-select"
-            value={selectedSession}
-            label="Session"
-            onChange={handleSessionChange}
-          >
-            <MenuItem value="2022/2023">2022/2023</MenuItem>
-            <MenuItem value="2023/2024">2023/2024</MenuItem>
-          </Select>
-        </FormControl>
+    <Grid item xs={12} sm={12} md={12} lg={12}>
+      <Paper elevation={3} style={{ padding: 20 }}>
+        <Typography variant="h6" gutterBottom>
+          Check Result
+        </Typography>
+        <Box sx={{ width: "100%", margin: "auto" }}>
+          <FormControl sx={{ m: 1, minWidth: "100%" }}>
+            <InputLabel id="session-label">Session</InputLabel>
+            <Select
+              labelId="session-label"
+              id="session-select"
+              value={selectedSession}
+              label="Session"
+              onChange={handleSessionChange}
+            >
+              <MenuItem value="2022/2023">2022/2023</MenuItem>
+              <MenuItem value="2023/2024">2023/2024</MenuItem>
+            </Select>
+          </FormControl>
 
-        <FormControl sx={{ m: 1, minWidth: "100%" }}>
-          <InputLabel id="classroom-label">Classroom</InputLabel>
-          <Select
-            labelId="classroom-label"
-            id="classroom-select"
-            value={selectedClassroom}
-            label="Classroom"
-            onChange={handleClassroomChange}
-          >
-            <MenuItem value="Class A">Class A</MenuItem>
-            <MenuItem value="Class B">Class B</MenuItem>
-          </Select>
-        </FormControl>
+          <FormControl sx={{ m: 1, minWidth: "100%" }}>
+            <InputLabel id="classroom-label">Classroom</InputLabel>
+            <Select
+              labelId="classroom-label"
+              id="classroom-select"
+              value={selectedClassroom}
+              label="Classroom"
+              onChange={handleClassroomChange}
+            >
+              <MenuItem value="Class A">Class A</MenuItem>
+              <MenuItem value="Class B">Class B</MenuItem>
+            </Select>
+          </FormControl>
 
-        <LoadingButton
-          variant="contained"
-          type="submit"
-          onClick={handleCheckResult}
-          loading={loading}
-          loadingPosition="start"
-          sx={{ mt: 3, mb: 2, width: "20%" }}
-        >
-          {loading ? "Checking..." : "Check Result"}
-        </LoadingButton>
-      </Box>
-    </Paper>
+          <Grid item xs={6} sm={3} lg={3}>
+            <LoadingButton
+              variant="contained"
+              type="submit"
+              onClick={handleCheckResult}
+              loading={loading}
+              loadingPosition="start"
+              sx={{ mt: 3, mb: 2, width: "100%" }}
+            >
+              {loading ? "Checking..." : "Check Result"}
+            </LoadingButton>
+          </Grid>
+        </Box>
+      </Paper>
+    </Grid>
   );
 };
 export default GeneralResultForm;
