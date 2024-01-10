8ae4feb3e7dfd6ac0bbd6544f9566b683b298a5a
“Ademic” <adeeyomichael77@gmail.com>
2024-01-10 23:59:18 +0100

    introduced axios requst handler in place of fetch api

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
 import { useNavigate } from "react-router-dom";
-import { FormControl } from '@mui/material';
-import { registrationValidator } from '../utils/dataValidator';
-import { successToast, errorToast, warningToast } from '../utils/toastUtils';
-import { useState } from 'react';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { AUTH_ENDPOINTS } from '../../apiCalls/endpoints';
-
+import { FormControl } from "@mui/material";
+import { registrationValidator } from "../utils/dataValidator";
+import { successToast, errorToast, warningToast } from "../utils/toastUtils";
+import { useState } from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { AUTH_ENDPOINTS } from "../../apiCalls/endpoints";
+import requestHandler from "../../apiCalls/requestHandler";
 
 const RegisterForm = ({ onSubmit, onToggleForm }) => {
   const [loading, setLoading] = useState(false);
@@ -32,56 +32,51 @@ const RegisterForm = ({ onSubmit, onToggleForm }) => {
       confirmPassword: event.target.elements.confirmPassword.value,
       date_of_birth: event.target.elements.dob.value,
       phone: event.target.elements.phone.value,
-    }
-    const validated = registrationValidator(data)
-    if (validated[1] === 'error'){
-      errorToast(validated[0])
+    };
+    const validated = registrationValidator(data);
+    if (validated[1] === "error") {
+      errorToast(validated[0]);
       setLoading(false);
-    }
-    else if (validated[1] === 'warning'){
-      warningToast(validated[0])
+    } else if (validated[1] === "warning") {
+      warningToast(validated[0]);
       setLoading(false);
-    }
-    else{
-      
+    } else {
       // ======================= REGISTRATION =======================
-      const URL = AUTH_ENDPOINTS.REGISTER
+      const URL = AUTH_ENDPOINTS.REGISTER;
       try {
-          const response = await fetch(URL, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(validated),
-              mode: 'cors'
-          })
-          console.log(JSON.stringify(validated));
-          if(response.ok){
-            setLoading(false);
-            navigate("/success");
-            const message = 'Registration Successful!, check your email for activation link'
-            successToast(message);
-          }else{
-              const message = `Registration failed:, ${response.statusText}`
-              errorToast(message);
-              setLoading(false);
-
-          }
-      }
-      catch(error){
-          const message = `Error during registration: ${error}`
-          successToast(message);
+        const response = await requestHandler("post", URL, validated);
+        if (response) {
           setLoading(false);
+          navigate("/success");
+          const message =
+            "Registration Successful!, check your email for activation link";
+          successToast(message);
+        } else {
+          const message = `Registration failed: ${
+            response ? response.detail : "Unknown error"
+          }`;
+          errorToast(message);
+          setLoading(false);
+        }
+      } catch (error) {
+        const message = `Error during registration: ${error.message}`;
+        errorToast(message);
+        setLoading(false);
       }
     }
   };
- 
+
   return (
-    <Box component="form" noValidate onSubmit={handleRegister} sx={{ mt: 5, width: '100%' }}>
-    {/* First Name and Last Name fields */}
-    <Grid container spacing={2}>
+    <Box
+      component="form"
+      noValidate
+      onSubmit={handleRegister}
+      sx={{ mt: 5, width: "100%" }}
+    >
+      {/* First Name and Last Name fields */}
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-        <TextField
+          <TextField
             autoComplete="fname"
             name="firstName"
             required
@@ -89,72 +84,71 @@ const RegisterForm = ({ onSubmit, onToggleForm }) => {
             id="firstName"
             label="First Name"
             autoFocus
-        />
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
-        <TextField
+          <TextField
             autoComplete="lname"
             name="lastName"
             required
             fullWidth
             id="lastName"
             label="Last Name"
-        />
-        </Grid>
-    </Grid>
-    
-    <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="phone"
-                label="Phone"
-                name="phone"
-                autoComplete="phone"
-            />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="dob"
-                type="date"
-                id="dob"
-                autoComplete="dob"
-            />
+          />
         </Grid>
       </Grid>
-    <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="registerEmail"
-                label="Email Address"
-                name="registerEmail"
-                autoComplete="email"
-            />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="username"
-                label="Username"
-                type="text"
-                id="username"
-                autoComplete="username"
-            />
-        </Grid>
-    </Grid>
 
-    {/* Role and Gender selection fields */}
-    <Grid container spacing={2}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="phone"
+            label="Phone"
+            name="phone"
+            autoComplete="phone"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="dob"
+            type="date"
+            id="dob"
+            autoComplete="dob"
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="registerEmail"
+            label="Email Address"
+            name="registerEmail"
+            autoComplete="email"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="username"
+            label="Username"
+            type="text"
+            id="username"
+            autoComplete="username"
+          />
+        </Grid>
+      </Grid>
+
+      {/* Role and Gender selection fields */}
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <FormControl fullWidth margin="normal" required>
             <InputLabel id="role-label">Role</InputLabel>
@@ -186,9 +180,9 @@ const RegisterForm = ({ onSubmit, onToggleForm }) => {
             </Select>
           </FormControl>
         </Grid>
-    </Grid>
-    {/* Password and Confirm Password fields */}
-    <Grid container spacing={2}>
+      </Grid>
+      {/* Password and Confirm Password fields */}
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <TextField
             margin="normal"
@@ -222,10 +216,9 @@ const RegisterForm = ({ onSubmit, onToggleForm }) => {
         variant="contained"
         sx={{ mt: 3, mb: 2 }}
       >
-        {loading ? 'Registering...' : 'Register'}
+        {loading ? "Registering..." : "Register"}
       </LoadingButton>
-</Box>
-
+    </Box>
   );
 };
 
