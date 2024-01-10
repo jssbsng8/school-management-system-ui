f1b8589367de03b9e16674d741b5ef7252566cf9
“Ademic” <adeeyomichael77@gmail.com>
2024-01-11 00:16:23 +0100

    introduced axios requst handler in place of fetch api

--- src/data/chartData.js
+++ src/data/chartData.js
@@ -82,11 +82,11 @@ export const lineChartOptions = {
 
 export const barChartData = [
   {
-    name: "Total Sales",
+    name: "Half Payment",
     data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
   },
   {
-    name: "Net Profit",
+    name: "Full Payment",
     data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
   },
 ];
