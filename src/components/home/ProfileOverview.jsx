119826c0aaac324f758de60ef0f7ed502b72c64a
“Ademic” <adeeyomichael77@gmail.com>
2024-01-10 23:59:58 +0100

    introduced axios requst handler in place of fetch api

--- src/components/home/ProfileOverview.jsx
+++ src/components/home/ProfileOverview.jsx
@@ -1,4 +1,4 @@
-import { Paper, Typography, Grid } from "@mui/material";
+import { Paper, Typography, Grid, Avatar } from "@mui/material";
 import { useUser } from "../utils/userContext";
 
 const ProfileOverview = ({ fullName, attendancePercentage, userName }) => {
@@ -6,43 +6,39 @@ const ProfileOverview = ({ fullName, attendancePercentage, userName }) => {
   return (
     <Paper elevation={3} style={{ padding: 20 }}>
       <Grid container spacing={2} alignItems="center">
-        <Grid item xs={12} sm={4}>
-          <img
+        <Grid item xs={12} sm={6} md={4}>
+          <Avatar
             src={localStorage.getItem("userImage")}
-            alt="Student"
-            style={{ width: "100%", borderRadius: "50%" }}
+            sx={{ width: 200, height: 200 }}
           />
         </Grid>
 
-        {role === "Student" && (
-          <Grid item xs={12} sm={8}>
-            <Typography variant="h4" gutterBottom>
-              Welcome, {fullName}! Keep Going.
-            </Typography>
-            <Typography paragraph>
-              Your current attendance is {attendancePercentage.toFixed(2)}%,
-              which is{" "}
-              {attendancePercentage > 75 ? "above" : "below or equal to"} 75.00%
-              of the minimum attendance mark.
-            </Typography>
-          </Grid>
-        )}
-
-        {role === "Teacher" && (
-          <Grid item xs={12} sm={8}>
-            <Typography variant="h4" gutterBottom>
-              Welcome, Mr. {userName.toUpperCase()}! Ready to Inspire.
-            </Typography>
-            <Typography paragraph>
-              You are currently assigned to teach{" "}
-              <span style={{ color: "green" }}>Mathematics</span> and{" "}
-              <span style={{ color: "green" }}>Biology.</span> Keep track of
-              your class schedules, manage student performance, and mark
-              attendance to ensure a productive learning environment. Your
-              dedication makes a difference!
-            </Typography>
-          </Grid>
-        )}
+        <Grid item xs={12} sm={6} md={8}>
+          <Typography variant="h4" gutterBottom>
+            {role === "Student"
+              ? `Welcome, ${fullName}! Keep Going.`
+              : `Welcome, Mr. ${userName.toUpperCase()}! Ready to Inspire.`}
+          </Typography>
+          <Typography paragraph>
+            {role === "Student"
+              ? `Your current attendance is ${attendancePercentage.toFixed(
+                  2
+                )}%, which is ${
+                  attendancePercentage > 75 ? "above" : "below or equal to"
+                } 75.00% of the minimum attendance mark.`
+              : `You are currently assigned to teach 
+            ${
+              role === "Teacher" && (
+                <span style={{ color: "green" }}>Mathematics</span>
+              )
+            } and 
+            ${
+              role === "Teacher" && (
+                <span style={{ color: "green" }}>Biology.</span>
+              )
+            } Keep track of your class schedules, manage student performance, and mark attendance to ensure a productive learning environment. Your dedication makes a difference!`}
+          </Typography>
+        </Grid>
       </Grid>
     </Paper>
   );
