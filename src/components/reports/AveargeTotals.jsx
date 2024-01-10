4ce042ef05ff4d110d5fbc2a67c6e8c4cd465a96
“Ademic” <adeeyomichael77@gmail.com>
2024-01-11 00:35:39 +0100

    Code Refactor

--- src/components/reports/AveargeTotals.jsx
+++ src/components/reports/AveargeTotals.jsx
@@ -16,7 +16,7 @@ const AveargeTotals = () => {
         padding: "16px",
       }}
     >
-      <Typography variant="h5">Average Total Sales</Typography>
+      <Typography variant="h5">Average Total</Typography>
       <Typography variant="subtitle2" sx={{ opacity: 0.7 }}>
         2022-2023
       </Typography>
