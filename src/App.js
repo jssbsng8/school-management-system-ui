39583745d5677eb6289f339ec34c2e555ec5e349
ademic2022 <adeeyomichael77@gmail.com>
2024-01-18 14:23:36 +0100

    feat: create reusable success component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Sidebar from "./components/common/Sidebar";
-import Authentication from "./pages/Authentication"
+import Authentication from "./pages/Authentication";
 import Navbar from "./components/common/Navbar";
-import { SuccessPage, ResetLinkSent } from "./pages";
+import { SuccessPage, ResetLinkSent, ActivationComponent } from "./pages";
 import Footer from "./components/common/Footer";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useUser } from "./components/utils/userContext";
 import { roleRoutes } from "./components/routing/userRoutes";
-
+// import { ActivationComponent } from "./pages";
 const sideBarWidth = 250;
 
 function App() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const {auth, role} = useUser();
+  const { auth, role } = useUser();
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -45,21 +45,42 @@ function App() {
           >
             {/* Routes */}
             <Routes>
-            {userRoutes.map((route) => (
-              <Route key={route.id} path={route.path} element={route.element} />
-            ))}
+              {userRoutes.map((route) => (
+                <Route
+                  key={route.id}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
             </Routes>
             <Footer />
           </Box>
         </Box>
       ) : (
         <>
-        <Routes>
-          <Route path="/login" element={<Authentication />} />
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/reset_password" element={<ResetLinkSent />} />
-        </Routes>
-        <ToastContainer />
+          <Routes>
+            <Route path="/login" element={<Authentication />} />
+            <Route
+              path="/success"
+              element={
+                <SuccessPage
+                  color="green"
+                  title="Registration Successful"
+                  body="We appreciate your registration."
+                  message="An email with further instructions has been sent to your email
+                  address."
+                  buttonText="Go to Login"
+                  linkTo="/login"
+                />
+              }
+            />
+            <Route path="/reset_password" element={<ResetLinkSent />} />
+            <Route
+              path="/activation/:uid/:token/"
+              element={<ActivationComponent />}
+            ></Route>
+          </Routes>
+          <ToastContainer />
         </>
       )}
     </>
