e84df4b5092fdd54d0fceca002b11533a8a73b8f
ademic2022 <adeeyomichael77@gmail.com>
2024-02-02 11:41:06 +0100

    feat: enhance All Users page with additional features

---
+++ src/components/utils/actionDropDownAllUsers.js
@@ -0,0 +1,36 @@
+export const ActionsDropdown = ({ id, onActionSelect, rows }) => {
+  const userIndex = rows ? rows.findIndex((row) => row.id === id) : -1;
+  const isUserActive = userIndex !== -1 && rows[userIndex].is_active;
+  const userRole = rows[userIndex].role;
+
+  const handleAction = (action) => {
+    onActionSelect(action, id);
+  };
+
+  return (
+    <select
+      onChange={(e) => handleAction(e.target.value)}
+      defaultValue=""
+      style={{
+        width: "100%",
+        height: "30px",
+        background: "#0a0d1f",
+        color: "#fff",
+        border: "none",
+        borderRadius: "8px",
+        padding: "0 5px",
+      }}
+    >
+      <option value="" disabled hidden>
+        Select Action
+      </option>
+      {!isUserActive && (
+        <option value="resendActivation">Resend Activation Link</option>
+      )}
+      <option value="deleteUser">Delete User Permanently</option>
+      {isUserActive && userRole === "Admin" && (
+        <option value="makeSuperAdmin">Make Super Admin</option>
+      )}
+    </select>
+  );
+};
