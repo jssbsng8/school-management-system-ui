42b9d1370acc64958edb8ef0196d8fccc91fc811
“Ademic” <adeeyomichael77@gmail.com>
2024-02-23 10:52:43 +0100

    refactor: refactor state management to use reducer

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -10,15 +10,25 @@ import { useNavigate } from "react-router-dom";
 import { AUTH_ENDPOINTS, USER_ENDPOINTS } from "../../apiCalls/endpoints";
 import { useUser } from "../utils/userContext";
 import requestHandler from "../../apiCalls/requestHandler";
+import ACTIONS from "../Reducer/actions";
+import { LoginReducer } from "../Reducer/loginReducer";
+
+const initialState = {
+  auth: false,
+  loading: false,
+  token: "",
+  user: {},
+  role: "",
+};
 
 const LoginForm = ({ onSubmit, onToggleForm }) => {
+  const [state, dispatch] = useReducer(LoginReducer, initialState);
   const { setUserContext } = useUser();
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
 
   const handleLogin = async (event) => {
     event.preventDefault();
-    setLoading(true);
+    dispatch({ type: ACTIONS.SET_LOADING, payload: true });
 
     // Extracting login data from the form
     const data = {
@@ -30,7 +40,7 @@ const LoginForm = ({ onSubmit, onToggleForm }) => {
     const validated = loginDataValidator(data);
     if (validated[1] === "error") {
       errorToast(validated[0]);
-      setLoading(false);
+      dispatch({ type: ACTIONS.LOGIN_ERROR, payload: false });
     } else {
       const apiUrl = AUTH_ENDPOINTS.LOGIN;
 
@@ -41,40 +51,48 @@ const LoginForm = ({ onSubmit, onToggleForm }) => {
           // Handle the case where response is undefined or null
           const message = response[1].data.non_field_errors[0];
           errorToast(message);
-          setLoading(false);
+          dispatch({ type: ACTIONS.LOGIN_ERROR, payload: false });
           return;
         }
 
         // Successful login
         // Storing the token or user information in local storage or state
-        localStorage.setItem("token", response[0].auth_token);
+        dispatch({ type: ACTIONS.SET_TOKEN, payload: response[0].auth_token });
+
+        await new Promise((resolve) => setTimeout(resolve, 100));
 
         // Fetching authenticated user data
         const loggedInUserData = await requestHandler(
           "get",
           USER_ENDPOINTS.AUTHENTICATED_USER
         );
-        localStorage.setItem(
-          "userData",
-          JSON.stringify({
-            auth: true,
-            role: loggedInUserData[0].role,
-          })
-        );
-        // Setting user context
-        setUserContext(loggedInUserData[0], true, loggedInUserData[0].role);
-
-        await new Promise((resolve) => setTimeout(resolve, 300));
-        // Navigating to the home page
-        const message = "Login successful";
-        successToast(message.toUpperCase());
-        setLoading(false);
-        navigate("/");
+        if (loggedInUserData[0]) {
+          localStorage.setItem(
+            "userData",
+            JSON.stringify({
+              auth: true,
+              role: loggedInUserData[0].role,
+            })
+          );
+          
+          dispatch({
+            type: ACTIONS.SUCCESSFUL_LOGIN,
+            payload: {
+              userData: loggedInUserData[0],
+              token: response[0].auth_token,
+              setUserContext: setUserContext
+            },
+          });
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          const message = "Login successful";
+          successToast(message.toUpperCase());
+          navigate("/");
+        }
       } catch (error) {
         const message = `${error.message}`;
         console.log(message);
         errorToast("Temporary Server Issue");
-        setLoading(false);
+        dispatch({ type: ACTIONS.LOGIN_ERROR, payload: false });
       }
     }
   };
@@ -91,6 +109,7 @@ const LoginForm = ({ onSubmit, onToggleForm }) => {
         name="email"
         autoComplete="email"
         autoFocus
+        defaultValue={"example2@gmail.com"}
       />
       <TextField
         margin="normal"
@@ -101,6 +120,7 @@ const LoginForm = ({ onSubmit, onToggleForm }) => {
         type="password"
         id="password"
         autoComplete="current-password"
+        defaultValue={"string@1234"}
       />
       <FormControlLabel
         control={<Checkbox value="remember" color="primary" />}
@@ -109,12 +129,12 @@ const LoginForm = ({ onSubmit, onToggleForm }) => {
       <LoadingButton
         type="submit"
         fullWidth
-        loading={loading}
+        loading={state.loading}
         loadingPosition="start"
         variant="contained"
         sx={{ mt: 3, mb: 2 }}
       >
-        {loading ? "Please Wait..." : "Sign In"}
+        {state.loading ? "Please Wait..." : "Sign In"}
       </LoadingButton>
     </Box>
   );
